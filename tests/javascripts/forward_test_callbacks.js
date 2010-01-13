deec67ce0d592cc0e1322f53aa49ae43529c14e9
Andrew Plummer <aplummer@cerego.co.jp>
2010-01-13 13:15:46 +0900

    first commit... adding everything

---
+++ tests/javascripts/forward_test_callbacks.js
@@ -0,0 +1,30 @@
+
+(function(){
+
+  var environment;
+
+  forwardCallback('log');
+  forwardCallback('testStart');
+  forwardCallback('testDone');
+  forwardCallback('moduleStart');
+  forwardCallback('moduleDone');
+  forwardCallback('done');
+
+  function forwardCallback(name){
+    if(!window.parent || !window.parent.QUnit || !window.parent.QUnit[name]) return;
+    QUnit[name] = function(){
+      var args = [];
+      for(var i=0;i<arguments.length;i++){ args.push(arguments[i]); }
+      args.push(environment);
+      window.parent.QUnit[name].apply(this, args);
+    }
+  }
+
+  registerEnvironment = function(name){
+    if(window.parent && window.parent.registerEnvironment){
+      environment = name;
+      window.parent.registerEnvironment(name);
+    }
+  }
+
+})();
