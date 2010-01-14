441f4d5a351d5ebbf3af82a48bade2a70d80592f
Andrew Plummer <aplummer@cerego.co.jp>
2010-01-14 13:09:26 +0900

    starting to develop! (TDD of course)

--- tests/environments/mootools/mootools-tests.js
+++ tests/environments/mootools/mootools-tests.js
@@ -1,12 +1,7 @@
-
-
 module("Mootools");
-//test("go fuck youreself", function () {
-//    equals($('hey'), document.getElementById('hey'));
-//});
 
 test("Basic Selectors", function () {
-    //equals(QUnit.equiv({}, {}), true);
+
     var main = $('main');
     equals(main.id, 'main', 'id');
     equals(main.getElements('a').length, 5, 'links');
@@ -16,23 +11,5 @@ test("Basic Selectors", function () {
     equals(main.getElements('[target=_blank]').length, 1, 'by target');
     equals(main.getElements('[target^=_bl]').length, 2, 'by beginning');
     equals(main.getElements('[rel$=nuts]').length, 1, 'by end');
-    equals(main.getElements('[rel$=nuts]').length, 5, 'how big could this message POSSIBLY be???');
-});
 
-//Returns all input tags with name "dialog".
-//$('myElement').getElements('input[name=dialog]');
-// 
-// //Returns all input tags with names ending with 'log'.
-// $('myElement').getElements('input[name$=log]');
-//  
-//  //Returns all email links (starting with "mailto:").
-//  $('myElement').getElements('a[href^=mailto:]');
-//   
-//   //Adds events to all Elements with the class name 'email'.
-//   $(document.body).getElements('a.email').addEvents({
-  //       'mouseenter': function(){
-    //               this.href = 'real@email.com';
-    //                   },
-    //                       'mouseleave': function(){
-      //                               this.href = '#';
-      //                                   }
+});
--- tests/tests/sugar.js
+++ tests/tests/sugar.js
@@ -1,7 +1,18 @@
 module("Sugar");
 
-test("Arrays", function () {
+test("Array", function () {
 
   equals(['a','b','c'].indexOf('b'), 1, 'Array#indexOf');
+  equals(['a','b','c'].indexOf('f'), -1, 'Array#indexOf');
+
+});
+
+test("Number", function () {
+
+  equals((1).odd(), true, 'Number#odd');  // 1 is odd
+  equals((2).odd(), false, 'Number#odd'); // 2 is not odd
+
+  equals((1).even(), false, 'Number#even');  // 1 is not even
+  equals((2).even(), true, 'Number#even');   // 2 is even
 
 });
