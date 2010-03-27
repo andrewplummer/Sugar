030f9eb9e06ebb6e6192f38df042e4b942133340
U-KOOPA\Bowser <Bowser@koopa.(none)>
2010-03-28 05:56:15 +0900

    WOW .... whole lotta goodness!
    Former-commit-id: e5327dfd7b69f0ade6e52dc3dc66304658158607

--- test/tests/sugar.js
+++ test/tests/sugar.js
@@ -72,27 +72,43 @@ test("String", function () {
   equal(counter, 2, 'String#each');
 
   counter = 0;
-  var letters = ['g','i','g','e'];
+  test = ['g','i','g','e'];
   'ginger'.each(/[a-i]/g, function(str, i){
-    equal(str, letters[i], 'String#each');
+    equal(str, test[i], 'String#each');
     counter++;
   });
   equal(counter, 4, 'String#each');
 
   counter = 0;
+  test = ['three', 'two', 'one', 'contact'];
+  'three|two|one|contact'.each('|', 'split', function(str, i){
+    equal(str, test[i], 'String#each');
+    counter++;
+  });
+  equal(counter, 4, 'String#each');
+
+  counter = 0;
+  test = ['beebop', 'rocksteady', 'and donatello'];
+  'beebop, rocksteady, and donatello'.each(/,\s*/g, 'split', function(str, i){
+    equal(str, test[i], 'String#each');
+    counter++;
+  });
+  equal(counter, 3, 'String#each');
+
+  counter = 0;
   var sentence = 'these pretzels are \n\n making me         thirsty!';
-  var wordArray = ['these', 'pretzels', 'are', 'making', 'me', 'thirsty!'];
+  test = ['these', 'pretzels', 'are', 'making', 'me', 'thirsty!'];
   sentence.eachWord(function(str, i){
-    equal(str, wordArray[i], 'String#eachWord');
+    equal(str, test[i], 'String#eachWord');
     counter ++;
   });
   equal(counter, 6, 'String#eachWord');
 
   counter = 0;
   var paragraph = 'these\npretzels\nare\n\nmaking\nme\n         thirsty!';
-  var lineArray = ['these', 'pretzels', 'are', '', 'making', 'me', '         thirsty!'];
+  test = ['these', 'pretzels', 'are', '', 'making', 'me', '         thirsty!'];
   paragraph.eachLine(function(str, i){
-    equal(str, lineArray[i], 'String#eachLine');
+    equal(str, test[i], 'String#eachLine');
     counter ++;
   });
   equal(counter, 7, 'String#eachLine');
@@ -104,16 +120,64 @@ test("String", function () {
   essay +=    'The British got their revenge in the late 60s with the British Invasion,\n';
   essay +=    'which claimed the lives of over 32,352 young women across the nation.\n\n\n\n\n';
   essay +=    'The End';
-  var paragraphArray = ['the history of the united states', 'it all began back in 1776 when someone declared something from someone.\nit was at this point that we had to get our rears in gear', 'The British got their revenge in the late 60s with the British Invasion,\nwhich claimed the lives of over 32,352 young women across the nation.', 'The End'];
+  test = ['the history of the united states', 'it all began back in 1776 when someone declared something from someone.\nit was at this point that we had to get our rears in gear', 'The British got their revenge in the late 60s with the British Invasion,\nwhich claimed the lives of over 32,352 young women across the nation.', 'The End'];
   essay.eachParagraph(function(str, i){
-    equal(str, paragraphArray[i], 'String#eachParagraph');
+    equal(str, test[i], 'String#eachParagraph');
     counter ++;
   });
   equal(counter, 4, 'String#eachParagraph');
 
 
+  equal('ō'.normalize(), 'o', 'String#normalize');
+  equal('o'.normalize(), 'o', 'String#normalize');
+  equal('kyōto'.normalize(), 'kyoto', 'String#normalize');
+  equal(''.normalize(), '', 'String#normalize');
+  equal('äěìøůŷñ'.normalize(), 'aeiouyn', 'String#normalize');
+
+  equal('Ō'.normalize(), 'O', 'String#normalize');
+  equal('KYŌTO'.normalize(), 'KYOTO', 'String#normalize');
+  equal('ÄĚÌØŮŶÑ'.normalize(), 'AEIOUYN', 'String#normalize');
 
 
+  equal('o'.accent('-'), 'ō', 'String#accent');
+  equal('a'.accent('`'), 'à', 'String#accent');
+  equal('a'.accent('v'), 'ǎ', 'String#accent');
+  equal('e'.accent(':'), 'ë', 'String#accent');
+  equal('e'.accent('-'), 'ē', 'String#accent');
+  equal('th'.accent(), 'þ', 'String#accent');
+  equal('dh'.accent(), 'ð', 'String#accent');
+  equal('ss'.accent(), 'ß', 'String#accent');
+  equal('oe'.accent(), 'œ', 'String#accent');
+
+  equal('A'.accent('`'), 'À', 'String#accent');
+  equal('A'.accent('v'), 'Ǎ', 'String#accent');
+  equal('E'.accent(':'), 'Ë', 'String#accent');
+  equal('E'.accent('-'), 'Ē', 'String#accent');
+
+
+
+  ok('hello'.startsWith('hell'), 'String#startsWith');
+  ok('HELLO'.startsWith('HELL'), 'String#startsWith');
+  ok('HELLO'.startsWith('hell'), 'String#startsWith');
+  ok('valley girls\nrock'.startsWith('valley girls'), 'String#startsWith');
+  equal('valley girls\nrock'.startsWith('valley girls r'), false, 'String#startsWith');
+
+
+  ok('vader'.endsWith('der'), 'String#endsWith');
+  ok('VADER'.endsWith('DER'), 'String#endsWith');
+  ok('VADER'.endsWith('der'), 'String#endsWith');
+  ok('i aint your\nfather'.endsWith('father'), 'String#endsWith');
+  equal('i aint your\nfather'.endsWith('r father'), false, 'String#endsWith');
+
+});
+
+test("RegExp", function () {
+
+  equals('test regexp', 'test regexp', 'RegExp#escape');
+  equals('test reg|exp', 'test reg\|exp', 'RegExp#escape');
+  equals('hey there (budday)', 'hey there \(budday\)', 'RegExp#escape');
+  equals('what a day...', 'what a day\.\.\.', 'RegExp#escape');
+  equals('*.+[]{}()?|/', '\*\.\+\[\]\{\}\(\)\?\|\/', 'RegExp#escape');
 
 });
 
