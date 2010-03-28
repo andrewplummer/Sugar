dbac55db3a194b16e0d5e5f05691ce667f308d6f
U-KOOPA\Bowser <Bowser@koopa.(none)>
2010-03-29 00:42:24 +0900

    hozhit...rolling now

--- test/javascripts/benchmark.js
+++ test/javascripts/benchmark.js
@@ -1,9 +1,11 @@
 
-var benchmarkMethod = function(obj, method){
+var benchmarkMethod = function(obj, method, iterations){
   var start = new Date();
-  for(var i=0;i<10000;i++){
-    obj[method]();
+  var args = Array.prototype.slice.call(arguments, 2);
+  iterations = iterations || 10000;
+  for(var i=0;i<iterations;i++){
+    obj[method](args);
   }
   var time = new Date() - start;
-  console.info('Time to complete 10,000 iterations: ' + time + 'ms');
+  console.info('Time to complete ' + iterations + ' iterations: ' + time + 'ms');
 }
