5e538ebe7baf4b0bb589f57bdcf21463bd100a37
U-KOOPA\Bowser <Bowser@koopa.(none)>
2010-04-18 19:21:05 +0900

    added Number#format
    Former-commit-id: d54b8e3b16dc80594fa26125524c1e164733c66b

--- test/javascripts/benchmark.js
+++ test/javascripts/benchmark.js
@@ -1,10 +1,10 @@
 
 var benchmarkMethod = function(obj, method, iterations){
   var start = new Date();
-  var args = Array.prototype.slice.call(arguments, 2);
+  var args = Array.prototype.slice.call(arguments, 3);
   iterations = iterations || 10000;
   for(var i=0;i<iterations;i++){
-    obj[method](args);
+    obj[method].apply(obj, args);
   }
   var time = new Date() - start;
   console.info('Time to complete ' + iterations + ' iterations: ' + time + 'ms');
