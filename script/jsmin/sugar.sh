4456bf81ba49199044648d1c24dd5397f3bad49e
Andrew Plummer <aplummer@cerego.co.jp>
2011-01-20 10:32:42 +0900

    script

--- script/jsmin/sugar.sh
+++ script/jsmin/sugar.sh
@@ -1,1 +1,1 @@
-java -jar script/jsmin/compiler.jar --js ../../lib/sugar.js --js_output_file ../../release/sugar.js
+java -jar script/jsmin/compiler.jar --js lib/sugar.js --js_output_file release/sugar.js
