b37febecfc1d0b555a3c5a41282e21f30211ba15
Andrew Plummer <aplummer@cerego.co.jp>
2011-07-09 21:54:47 +0900

    adding setFlags, addFlag, and removeFlag for regexes
    Former-commit-id: 351f5d3cf03b9190dec2f3b340f1c354011edf67 [formerly 6bf7e24af73194ab9ab7c0512f98c9f56fd50abd]
    Former-commit-id: 0050d1fbc4596983d10d58566ca13f31b7dcec9f

--- unit_tests/sugar/core.js
+++ unit_tests/sugar/core.js
@@ -4440,6 +4440,8 @@ test('Date', function () {
 
 test('RegExp', function () {
 
+  var r, n;
+
   equals(RegExp.escape('test regexp'), 'test regexp', 'RegExp#escape');
   equals(RegExp.escape('test reg|exp'), 'test reg\\|exp', 'RegExp#escape');
   equals(RegExp.escape('hey there (budday)'), 'hey there \\(budday\\)', 'RegExp#escape');
@@ -4447,6 +4449,38 @@ test('RegExp', function () {
   equals(RegExp.escape('.'), '\\.', 'RegExp#escape');
   equals(RegExp.escape('*.+[]{}()?|/'), '\\*\\.\\+\\[\\]\\{\\}\\(\\)\\?\\|\\/', 'RegExp#escape');
 
+  r = /foobar/;
+  n = r.setFlags('gim');
+
+  equals(n.global, true, 'RegExp#setFlags');
+  equals(n.ignoreCase, true, 'RegExp#setFlags');
+  equals(n.multiline, true, 'RegExp#setFlags');
+
+  equals(r.global, false, 'RegExp#setFlags | initial regex is untouched');
+  equals(r.ignoreCase, false, 'RegExp#setFlags | initial regex is untouched');
+  equals(r.multiline, false, 'RegExp#setFlags | initial regex is untouched');
+
+  n = r.addFlag('g');
+
+  equals(n.global, true, 'RegExp#addFlag');
+  equals(n.ignoreCase, false, 'RegExp#addFlag');
+  equals(n.multiline, false, 'RegExp#addFlag');
+
+  equals(r.global, false, 'RegExp#addFlag | initial regex is untouched');
+  equals(r.ignoreCase, false, 'RegExp#addFlag | initial regex is untouched');
+  equals(r.multiline, false, 'RegExp#addFlag | initial regex is untouched');
+
+
+  r = /foobar/gim;
+  n = r.removeFlag('g');
+
+  equals(n.global, false, 'RegExp#removeFlag');
+  equals(n.ignoreCase, true, 'RegExp#removeFlag');
+  equals(n.multiline, true, 'RegExp#removeFlag');
+
+  equals(r.global, true, 'RegExp#removeFlag | initial regex is untouched');
+  equals(r.ignoreCase, true, 'RegExp#removeFlag | initial regex is untouched');
+  equals(r.multiline, true, 'RegExp#removeFlag | initial regex is untouched');
 });
 
 test('Object', function () {
