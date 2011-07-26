4f116624e34539408a439ac7d5dcfe1a507c09f7
Andrew Plummer <plummer.andrew@gmail.com>
2011-07-27 01:34:42 +0900

    1. indexOf and lastIndexOf + tests 3. Next is #every 4. some and map 5. filter 6. reduce and reduceRight done 7. extensive testing for indexOf... still not sure what to do about sparse arrays 8. much going back and forth but finally getting a good direction on the ES5 spec.

--- unit_tests/sugar/es5.js
+++ unit_tests/sugar/es5.js
@@ -4,9 +4,7 @@ var windowOrUndefined = (typeof window !== 'undefined' ? window : undefined);
 
 test('ECMAScript', function () {
 
-  var arr, count, expected, result;
-
-
+  var arr, count, expected, result, previous, current;
 
 
 
@@ -14,11 +12,13 @@ test('ECMAScript', function () {
 
   arr = ['a','b','c'];
 
-  raisesError(function(){ arr.forEach(); }, 'Array#forEach | should raise an error when no fn given');
-
+  raisesError(function(){ [].forEach(); }, 'Array#forEach | should raise an error when no fn given');
   result = arr.forEach(function(){
      equals(this, windowOrUndefined, 'Array#forEach | scope should be undefined when not passed');
   });
+  result = arr.forEach(function(){
+    equals(this, 'wasabi', 'Array#forEach | scope can be passed');
+  }, 'wasabi');
   equals(result, undefined, 'Array#forEach | returns undefined');
 
   arr[234] = 'd';
@@ -27,7 +27,6 @@ test('ECMAScript', function () {
   arr.forEach(function(el, i, arr){
     arr.push(3)
     equals(el, expected[count], 'Array#forEach | elements should be as expected');
-    equals(this, 'wasabi', 'Array#forEach | scope can be passed');
     equals(typeof i, 'number', 'Array#forEach | i must be a number');
     count++;
   }, 'wasabi');
@@ -73,7 +72,6 @@ test('ECMAScript', function () {
     equals(el, expected[count], 'Array#forEach | elements should be as expected');
     count++;
   });
-
   equals(count, 2, 'Array#forEach | elements deleted after the loop begins should not be visited');
 
   arr = [];
@@ -87,6 +85,485 @@ test('ECMAScript', function () {
   });
 
   equals(count, 1, 'Array#forEach | setting array elements with a string is the same as with a number');
+
+
+  arr = [];
+  arr[2] = 'c';
+  arr[1] = 'b';
+  arr[0] = 'a';
+
+  result = [];
+  arr.forEach(function(el) {
+    result.push(el);
+  });
+  same(result, ['a','b','c'], 'Array#forEach | walks array in order');
+
+
+  // #indexOf
+
+  arr = [1,2,3];
+  arr[-2] = 4; // Throw a wrench in the gears by assigning a non-valid array index as object property.
+
+  equals(arr.indexOf(1), 0, 'Array#indexOf | finds 1');
+  equals(arr.indexOf(1) === 0, true, 'Array#indexOf | finds 1 and is result strictly equal');
+  equals(arr.indexOf(4), -1, 'Array#indexOf | does not find 4');
+  equals(arr.indexOf('1'), -1, 'Array#indexOf | Uses strict equality');
+  equals(arr.indexOf(2, 1), 1, 'Array#indexOf | from index 1');
+  equals(arr.indexOf(2, 2), -1, 'Array#indexOf | from index 2');
+  equals(arr.indexOf(2, 3), -1, 'Array#indexOf | from index 3');
+  equals(arr.indexOf(2, 4), -1, 'Array#indexOf | from index 4');
+  equals(arr.indexOf(3, -1), 2, 'Array#indexOf | from index -1');
+  equals(arr.indexOf(3, -2), 2, 'Array#indexOf | from index -2');
+  equals(arr.indexOf(3, -3), 2, 'Array#indexOf | from index -3');
+  equals(arr.indexOf(3, -4), 2, 'Array#indexOf | from index -4');
+
+  // These tests will by proxy be stress testing the toInteger internal private function.
+  equals(arr.indexOf(1, NaN), 0, 'Array#indexOf | index NaN becomes 0');
+  equals(arr.indexOf(1, true), -1, 'Array#indexOf | index true becomes 1');
+  equals(arr.indexOf(1, false), 0, 'Array#indexOf | index false becomes 0');
+  equals(arr.indexOf(1, 0.1), 0, 'Array#indexOf | index 0.1 becomes 0');
+  equals(arr.indexOf(1, 1.1), -1, 'Array#indexOf | index 1.1 becomes 1');
+  equals(arr.indexOf(3, -0.1), 2, 'Array#indexOf | index -0.1 becomes 0');
+  equals(arr.indexOf(3, -1.1), 2, 'Array#indexOf | index -1.1 becomes -1');
+  equals(arr.indexOf(1, 1.7), -1, 'Array#indexOf | index 1.7 becomes 1');
+  equals(arr.indexOf(3, -1.7), 2, 'Array#indexOf | index -1.7 becomes -1');
+
+  arr = [];
+  arr[2] = 'c';
+  arr[1] = 'c';
+  arr[0] = 'c';
+
+  equals(arr.indexOf('c'), 0, 'Array#indexOf | walks array in order');
+
+
+  // Although Infinity appears to be allowable in the ECMA spec, both of these cases
+  // would appear to kill all modern browsers.
+  // equals(arr.indexOf(1, Infinity), -1, 'Array#indexOf | infinity is valid');  This locks the browser... should it??
+  // equals(arr.indexOf(1, -Infinity), 0, 'Array#indexOf | -infinity is valid');
+
+
+  // #lastIndexOf
+
+  arr = ['a', 1, 'a'];
+  arr[-2] = 'a'; // Throw a wrench in the gears by assigning a non-valid array index as object property.
+
+  equals(arr.lastIndexOf('a'), 2, 'Array#lastIndexOf | finds a');
+  equals(arr.lastIndexOf('a') === 2, true, 'Array#lastIndexOf | finds a and is result strictly equal');
+  equals(arr.lastIndexOf('c'), -1, 'Array#lastIndexOf | does not find c');
+  equals(arr.lastIndexOf('1'), -1, 'Array#lastIndexOf | Uses strict equality');
+  equals(arr.lastIndexOf('a', 1), 0, 'Array#lastIndexOf | from index 1');
+  equals(arr.lastIndexOf('a', 2), 2, 'Array#lastIndexOf | from index 2');
+  equals(arr.lastIndexOf('a', 3), 2, 'Array#lastIndexOf | from index 3');
+  equals(arr.lastIndexOf('a', 4), 2, 'Array#lastIndexOf | from index 4');
+  equals(arr.lastIndexOf('a', 0), 0, 'Array#lastIndexOf | from index 0');
+  equals(arr.lastIndexOf('a', -1), 2, 'Array#lastIndexOf | from index -1');
+  equals(arr.lastIndexOf('a', -2), 0, 'Array#lastIndexOf | from index -2');
+  equals(arr.lastIndexOf('a', -3), 0, 'Array#lastIndexOf | from index -3');
+  equals(arr.lastIndexOf('a', -4), -1, 'Array#lastIndexOf | from index -4');
+
+  arr = [];
+  arr[2] = 'c';
+  arr[1] = 'c';
+  arr[0] = 'c';
+
+  equals(arr.lastIndexOf('c'), 2, 'Array#lastIndexOf | walks array in order');
+
+
+
+  // #every
+
+  raisesError(function(){ [].every(); }, 'Array#every | should raise an error when no first param');
+  result = arr.every(function(){
+     equals(this, windowOrUndefined, 'Array#every | scope should be undefined when not passed');
+  });
+  [1].every(function(){
+    equals(this, 'wasabi', 'Array#every | scope can be passed');
+  }, 'wasabi');
+  [1].every(function(){
+    equals(this, '', 'Array#every | scope can be falsy');
+  }, '');
+  equals([].every(function(){ return true; }), true, 'Array#every | empty arrays will always be true');
+  equals([].every(function(){ return false; }), true, 'Array#every | empty arrays will always be true even when false returned');
+  equals([1].every(function(){ return 1; }), true, 'Array#every | 1 coerced to true');
+  equals([1].every(function(){ return 0; }), false, 'Array#every | 0 coerced to false');
+  equals([1].every(function(){ return 'blah'; }), true, 'Array#every | non-null string coerced to true');
+  equals([1].every(function(){ return ''; }), false, 'Array#every | blank string coerced to false');
+
+  arr = ['c','c','c'];
+  count = 0;
+  result = arr.every(function(el, i, a){
+    equals(el, 'c', 'Array#every | first argument is element');
+    equals(i, count, 'Array#every | second argument is index');
+    same(a, arr, 'Array#every | third argument is the array');
+    count++;
+    return el == 'c';
+  });
+  equals(result, true, 'Array#every | all are c');
+  equals(count, 3, 'Array#every | should have been called 3 times');
+
+
+  arr = ['a','b','c'];
+  count = 0;
+  result = arr.every(function(el){
+    count++;
+    return el == 'c';
+  });
+  equals(result, false, 'Array#every | not all are c');
+  equals(count, 1, 'Array#every | should stop once it can return false');
+
+
+  arr = [];
+  arr[247] = 'a';
+  count = 0;
+  result = arr.every(function(el){
+    count++;
+    return el == 'a';
+  });
+  equals(result, true, 'Array#every | sparse arrays should not count missing elements');
+  equals(count, 1, 'Array#every | sparse arrays should have called once only');
+
+
+  arr = ['a','b','c'];
+  expected = ['a','x'];
+  count = 0;
+  arr.every(function(el, i){
+    if(i == 0) {
+      arr[1] = 'x';
+      delete arr[2];
+    }
+    equals(el, expected[count], 'Array#every | elements should be as expected');
+    count++;
+    return true;
+  });
+  equals(count, 2, 'Array#every | elements deleted after the loop begins should not be visited');
+
+
+
+
+
+
+
+  // #some
+
+  raisesError(function(){ [].some(); }, 'Array#some | should raise an error when no first param');
+  result = arr.some(function(){
+     equals(this, windowOrUndefined, 'Array#some | scope should be undefined when not passed');
+  });
+  [1].some(function(){
+    equals(this, 'wasabi', 'Array#some | scope can be passed');
+  }, 'wasabi');
+  [1].some(function(){
+    equals(this, '', 'Array#some | scope can be falsy');
+  }, '');
+  equals([].some(function(){ return true; }), false, 'Array#some | empty arrays will always be false');
+  equals([].some(function(){ return false; }), false, 'Array#some | empty arrays will always be false even when false returned');
+  equals([1].some(function(){ return 1; }), true, 'Array#some | 1 coerced to true');
+  equals([1].some(function(){ return 0; }), false, 'Array#some | 0 coerced to false');
+  equals([1].some(function(){ return 'blah'; }), true, 'Array#some | non-null string coerced to true');
+  equals([1].some(function(){ return ''; }), false, 'Array#some | blank string coerced to false');
+
+  arr = ['c','c','c'];
+  count = 0;
+  result = arr.some(function(el, i, a){
+    equals(el, 'c', 'Array#some | first argument is element');
+    equals(i, count, 'Array#some | second argument is index');
+    same(a, arr, 'Array#some | third argument is the array');
+    count++;
+    return el == 'c';
+  });
+  equals(result, true, 'Array#some | some are c');
+  equals(count, 1, 'Array#some | should stop as soon as it finds an element');
+
+
+  arr = ['a','b','c'];
+  count = 0;
+  result = arr.some(function(el){
+    count++;
+    return el == 'd';
+  });
+  equals(result, false, 'Array#some | none are d');
+  equals(count, 3, 'Array#some | should have been called 3 times');
+
+
+  arr = [];
+  arr[247] = 'a';
+  count = 0;
+  result = arr.some(function(el){
+    count++;
+    return el == 'a';
+  });
+  equals(result, true, 'Array#some | sparse arrays should not count missing elements');
+  equals(count, 1, 'Array#some | sparse arrays should have called once only');
+
+
+  arr = ['a','b','c'];
+  expected = ['a','x'];
+  count = 0;
+  arr.some(function(el, i){
+    if(i == 0) {
+      arr[1] = 'x';
+      delete arr[2];
+    }
+    equals(el, expected[count], 'Array#some | elements should be as expected');
+    count++;
+    return false;
+  });
+  equals(count, 2, 'Array#some | elements deleted after the loop begins should not be visited');
+
+
+
+
+
+
+  // #map
+
+  raisesError(function(){ [].map(); }, 'Array#map | should raise an error when no first param');
+  result = arr.map(function(){
+     equals(this, windowOrUndefined, 'Array#map | scope should be undefined when not passed');
+  });
+  [1].map(function(){
+    equals(this, 'wasabi', 'Array#map | scope can be passed');
+  }, 'wasabi');
+  [1].map(function(){
+    equals(this, '', 'Array#map | scope can be falsy');
+  }, '');
+
+  arr = ['c','c','c'];
+  count = 0;
+  result = arr.map(function(el, i, a){
+    equals(el, 'c', 'Array#map | first argument is element');
+    equals(i, count, 'Array#map | second argument is index');
+    same(a, arr, 'Array#map | third argument is the array');
+    count++;
+    return 'a';
+  });
+  same(result, ['a','a','a'], 'Array#map | mapped to a');
+  equals(count, 3, 'Array#map | should have run 3 times');
+
+
+  arr = [1,2,3];
+  count = 0;
+  result = arr.map(function(el){
+    return Math.pow(el, 2);
+  });
+  same(result, [1,4,9], 'Array#map | n^2');
+
+
+  arr = [];
+  arr[247] = 'a';
+  count = 0;
+  result = arr.map(function(el){
+    count++;
+    return 'c';
+  });
+  same(result.length, 248, 'Array#map | resulting array should also be sparse if source was');
+  equals(count, 1, 'Array#map | callback should only have been called once');
+
+
+  arr = ['a','b','c'];
+  expected = ['a','x'];
+  count = 0;
+  arr.map(function(el, i){
+    if(i == 0) {
+      arr[1] = 'x';
+      delete arr[2];
+    }
+    equals(el, expected[count], 'Array#map | elements should be as expected');
+    count++;
+  });
+  equals(count, 2, 'Array#map | elements deleted after the loop begins should not be visited');
+
+
+
+
+
+  // #filter
+
+  raisesError(function(){ [].filter(); }, 'Array#filter | should raise an error when no first param');
+  result = arr.filter(function(){
+     equals(this, windowOrUndefined, 'Array#filter | scope should be undefined when not passed');
+  });
+  [1].filter(function(){
+    equals(this, 'wasabi', 'Array#filter | scope can be passed');
+  }, 'wasabi');
+  [1].filter(function(){
+    equals(this, '', 'Array#filter | scope can be falsy');
+  }, '');
+  same([].filter(function(){ return true; }), [], 'Array#filter | empty arrays will always be []');
+  same([].filter(function(){ return false; }), [], 'Array#filter | empty arrays will always be [] even when false returned');
+  same([1].filter(function(){ return 1; }), [1], 'Array#filter | 1 coerced to true');
+  same([1].filter(function(){ return 0; }), [], 'Array#filter | 0 coerced to false');
+  same([1].filter(function(){ return 'blah'; }), [1], 'Array#filter | non-null string coerced to true');
+  same([1].filter(function(){ return ''; }), [], 'Array#filter | blank string coerced to false');
+
+  arr = ['c','c','c'];
+  count = 0;
+  result = arr.filter(function(el, i, a){
+    equals(el, 'c', 'Array#filter | first argument is element');
+    equals(i, count, 'Array#filter | second argument is index');
+    same(a, arr, 'Array#filter | third argument is the array');
+    count++;
+    return el == 'c';
+  });
+  same(result, ['c','c','c'], 'Array#filter | filter are c');
+  equals(count, 3, 'Array#filter | should have executed 3 times');
+
+
+  arr = ['a','b','c'];
+  count = 0;
+  result = arr.filter(function(el){
+    count++;
+    return el == 'b';
+  });
+  same(result, ['b'], 'Array#filter | returns [b]');
+  equals(count, 3, 'Array#filter | should have been called 3 times');
+
+
+  arr = [];
+  arr[247] = 'a';
+  count = 0;
+  result = arr.filter(function(el){
+    count++;
+    return true;
+  });
+  same(result, ['a'], 'Array#filter | sparse arrays should not count missing elements');
+  equals(count, 1, 'Array#filter | sparse arrays should have called once only');
+
+
+  arr = ['a','b','c'];
+  expected = ['a','x'];
+  count = 0;
+  result = arr.filter(function(el, i){
+    if(i == 0) {
+      arr[1] = 'x';
+      delete arr[2];
+    }
+    equals(el, expected[count], 'Array#filter | elements should be as expected');
+    count++;
+    return true;
+  });
+  same(result, ['a','x'], 'Array#filter | modified array should appear as the result');
+  equals(count, 2, 'Array#filter | elements deleted after the loop begins should not be visited');
+
+
+
+
+  // #reduce
+
+  raisesError(function(){ [].reduce(); }, 'Array#reduce | should raise an error when no callback provided');
+  raisesError(function(){ [].reduce(function(){}); }, 'Array#reduce | should raise an error on an empty array with no initial value');
+  [1].reduce(function(){
+     equals(this, windowOrUndefined, 'Array#reduce | scope should be undefined');
+  }, 1);
+
+
+  arr = [1,2,3];
+  previous = [1,3];
+  current = [2,3];
+  count = 0;
+
+  result = arr.reduce(function(prev, el, i, o){
+    equals(prev, previous[count], 'Array#filter | first argument is the prev value');
+    equals(el, current[count], 'Array#filter | second argument is element');
+    equals(i, count + 1, 'Array#filter | third argument is index');
+    same(o, arr, 'Array#filter | fourth argument is the array');
+    count++;
+    return prev + el;
+  });
+
+  equals(result, 6, 'Array#reduce | result is correct');
+  equals(count, 2, 'Array#reduce | should have been called 3 times');
+
+
+  equals([1].reduce(function(){ return 324242; }), 1, 'Array#reduce | function is not called and returns 1');
+
+  count = 0;
+  [1].reduce(function(prev, current, i) {
+    equals(prev, 5, 'Array#reduce | prev is equal to the inital value if it is provided');
+    equals(current, 1, 'Array#reduce | current is equal to the first value in the array if no intial value provided');
+    equals(i, 0, 'Array#reduce | i is 0 when an initial value is passed');
+    count++;
+  }, 5);
+  equals(count, 1, 'Array#reduce | should have been called once');
+
+  arr = ['a','b','c'];
+  previous = ['a','ab'];
+  current  = ['b','c'];
+  count = 0;
+  result = arr.reduce(function(prev, el, i){
+    if(i == 0) {
+      arr[1] = 'x';
+      delete arr[2];
+    }
+    equals(prev, previous[count], 'Array#reduce | previous should be as expected');
+    equals(el, current[count], 'Array#reduce | current should be as expected');
+    count++;
+    return prev + el;
+  });
+  equals(count, 2, 'Array#reduce | elements deleted after the loop begins should not be visited');
+
+
+
+
+
+
+  // #reduceRight
+
+  raisesError(function(){ [].reduceRight(); }, 'Array#reduceRight | should raise an error when no callback provided');
+  raisesError(function(){ [].reduceRight(function(){}); }, 'Array#reduceRight | should raise an error on an empty array with no initial value');
+  [1].reduceRight(function(){
+     equals(this, windowOrUndefined, 'Array#reduceRight | scope should be undefined');
+  }, 1);
+
+
+  arr = [1,2,3];
+  previous = [3,5];
+  current = [2,1];
+  count = 0;
+
+  result = arr.reduceRight(function(prev, el, i, o){
+    equals(prev, previous[count], 'Array#filter | first argument is the prev value');
+    equals(el, current[count], 'Array#filter | second argument is element');
+    equals(i, 1 - count, 'Array#filter | third argument is index');
+    same(o, arr, 'Array#filter | fourth argument is the array');
+    count++;
+    return prev + el;
+  });
+
+  equals(result, 6, 'Array#reduceRight | result is correct');
+  equals(count, 2, 'Array#reduceRight | should have been called 3 times');
+
+
+  equals([1].reduceRight(function(){ return 324242; }), 1, 'Array#reduceRight | function is not called and returns 1');
+
+  count = 0;
+  [1].reduceRight(function(prev, current, i) {
+    equals(prev, 5, 'Array#reduceRight | prev is equal to the inital value if it is provided');
+    equals(current, 1, 'Array#reduceRight | current is equal to the first value in the array if no intial value provided');
+    equals(i, 0, 'Array#reduceRight | i is 0 when an initial value is passed');
+    count++;
+  }, 5);
+  equals(count, 1, 'Array#reduceRight | should have been called once');
+
+  arr = ['a','b','c'];
+  previous = ['c','cb'];
+  current  = ['b','a'];
+  count = 0;
+  result = arr.reduceRight(function(prev, el, i){
+    if(i == 0) {
+      arr[1] = 'x';
+      delete arr[2];
+    }
+    equals(prev, previous[count], 'Array#reduceRight | previous should be as expected');
+    equals(el, current[count], 'Array#reduceRight | current should be as expected');
+    count++;
+    return prev + el;
+  });
+  equals(count, 2, 'Array#reduceRight | elements deleted after the loop begins should not be visited');
+
+
+
+
 
   // #indexOf
 
