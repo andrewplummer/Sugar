385c6ac0b2d82b0552431720611caab5a8478555
Andrew Plummer <plummer.andrew@gmail.com>
2011-08-23 22:48:15 +0900

    1. refactored stripTags & removeTags 2. added a simpler mechanism for iterating over method arguments 3. Array#remove now handles multiple arguments 4. Array#exclude now handles multiple arguments 5. Array#union now handles multiple arguments 6. Array#intersect now handles multiple arguments 7. Array#subtract now handles multiple arguments
    Former-commit-id: 372622603c056c02ffcc223ddbe243b900890bf5 [formerly 1db86bde7292c32137ab5363d2984d10d786e3ee]
    Former-commit-id: e8f5a200b5aed7636ff8c5845e4e853a01d1a13a

--- unit_tests/environments/underscore/adapter.js
+++ unit_tests/environments/underscore/adapter.js
@@ -7,14 +7,83 @@ ok = function(actual, message) {
   equal(actual, true, message);
 }
 
+var ensureArray = function(obj) {
+  return Object.isArray(obj) ? obj : Array.prototype.slice.call(obj);
+}
 
 var CompatibleMethods = [
   {
     module: Array.prototype,
     methods: [
-      'first',
-      'last',
-      'compact'
+      {
+        name: 'first',
+        method: function(arr, n){
+          return ensureArray(arr).first(n);
+        }
+      },
+      {
+        name: 'last',
+        method: function(arr, n){
+          return ensureArray(arr).last(n);
+        }
+      },
+      {
+        name: 'rest',
+        method: function(arr, n){
+          if(n === undefined) n = 1;
+          return ensureArray(arr).from(n);
+        }
+      },
+      {
+        name: 'compact',
+        method: function(arr){
+          return ensureArray(arr).compact(true);
+        }
+      },
+      {
+        name: 'flatten',
+        method: function(arr){
+          return ensureArray(arr).flatten();
+        }
+      },
+      {
+        name: 'without',
+        method: function(arr){
+          arr = ensureArray(arr);
+          var args = Array.prototype.slice.call(arguments, 1);
+          return Array.prototype.exclude.apply(arr, args);
+        }
+      },
+      {
+        name: 'uniq',
+        method: function(arr){
+          return ensureArray(arr).unique();
+        }
+      },
+      {
+        name: 'intersection',
+        method: function(arr){
+          arr = ensureArray(arr);
+          var args = Array.prototype.slice.call(arguments, 1);
+          return Array.prototype.intersect.apply(arr, args);
+        }
+      },
+      {
+        name: 'union',
+        method: function(arr, a){
+          arr = ensureArray(arr);
+          var args = Array.prototype.slice.call(arguments, 1);
+          return Array.prototype.union.apply(arr, args);
+        }
+      },
+      {
+        name: 'difference',
+        method: function(arr, a){
+          arr = ensureArray(arr);
+          var args = Array.prototype.slice.call(arguments, 1);
+          return Array.prototype.subtract.apply(arr, args);
+        }
+      }
     ]
   }
 ];
@@ -23,9 +92,7 @@ var mapMethods = function() {
   var proto;
   CompatibleMethods.forEach(function(cm) {
     cm.methods.forEach(function(m) {
-      _[m] = function(obj){
-        return cm.module[m].apply(obj, Array.prototype.slice.call(arguments, 1));
-      }
+      _[m.name] = m.method;
     });
   });
 }
