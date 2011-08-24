e80e13ca6ee00bf0b27450e31c8b43b1c3bc9ad3
Andrew Plummer <plummer.andrew@gmail.com>
2011-08-25 01:55:16 +0900

    refactored storeMethods now that there is no discernable collision for class/instance methods

--- unit_tests/environments/prototype/override/sugar.js
+++ unit_tests/environments/prototype/override/sugar.js
@@ -1,17 +1,40 @@
 
 test('Array', function () {
 
-  Array.sugar(false, 'max');
 
-  equal([12,87,55].max(), 87, 'Array#max | Prototype still has control');
+  // Just the method name will find the proper location
 
-  Array.sugar(true, 'max');
+  notEqual([].max, Array.SugarMethods['max'].method, 'Array#max does not belong to Sugar');
+  Array.sugar('max');
+  strictlyEqual([].max, Array.SugarMethods['max'].method, 'Array#max now belongs to Sugar');
 
-  equal([12,87,55].max(), [87], 'Array#max | Sugar regained control');
-  equal([12,87,55].min(), 12, 'Array#min | Prototype still has control');
+  // No parameters will hijack the entire module
 
-  Array.sugar('min');
+  notEqual((8).abs, Number.SugarMethods['abs'].method, 'Number#abs is Prototype');
+  notEqual((8).ceil, Number.SugarMethods['ceil'].method, 'Number#ceil is Prototype');
+  notEqual((8).floor, Number.SugarMethods['floor'].method, 'Number#floor is Prototype');
+  notEqual((8).round, Number.SugarMethods['round'].method, 'Number#round is Prototype');
 
-  equal([12,87,55].min(), [12], 'Array#min | Sugar regained control');
+  Number.sugar();
+
+  strictlyEqual((8).abs, Number.SugarMethods['abs'].method, 'Number#abs is now Sugar');
+  strictlyEqual((8).ceil, Number.SugarMethods['ceil'].method, 'Number#ceil is now Sugar');
+  strictlyEqual((8).floor, Number.SugarMethods['floor'].method, 'Number#floor is now Sugar');
+  strictlyEqual((8).round, Number.SugarMethods['round'].method, 'Number#round is now Sugar');
+
+
+  // Ambiguous argument will default to class method only.
+
+  notEqual(Object.keys, Object.SugarMethods['keys'].method, 'Object.keys is Prototype');
+  notEqual(Object.isDate, Object.SugarMethods['isDate'].method, 'Object.isDate is Prototype');
+  notEqual(Object.isNumber, Object.SugarMethods['isNumber'].method, 'Object.isNumber is Prototype');
+  notEqual(Object.isFunction, Object.SugarMethods['isFunction'].method, 'Object.isFunction is Prototype');
+
+  Object.sugar('keys', 'isDate', 'isNumber', 'somebooshit');
+
+  strictlyEqual(Object.keys, Object.SugarMethods['keys'].method, 'Object.keys is Sugar');
+  strictlyEqual(Object.isDate, Object.SugarMethods['isDate'].method, 'Object.isDate is Sugar');
+  strictlyEqual(Object.isNumber, Object.SugarMethods['isNumber'].method, 'Object.isNumber is Sugar');
+  notEqual(Object.isFunction, Object.SugarMethods['isFunction'].method, 'Object.isFunction is still Prototype');
 
 });
