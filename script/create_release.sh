1ece593618ba005bc6d57991020a5b97a01f3232
Andrew Plummer <plummer.andrew@gmail.com>
2011-10-12 02:39:10 +0900

    beginning on optimizations
    Former-commit-id: 46f29e7b4d1232835b2c9be4d2de352ae504f6a2 [formerly 0e7c70bd8c61d4a7293cec5fe353fab8c0fc81cc]
    Former-commit-id: 2b492f59516d1a2a5d41aa017201d2a7f99fa895

--- script/create_release.sh
+++ script/create_release.sh
@@ -1,5 +1,5 @@
 sed "s/VERSION/$1/" release/copyright.txt > release/tmp1.js
-java -jar script/jsmin/compiler.jar --js lib/sugar.js --js_output_file release/tmp2.js
+java -jar script/jsmin/compiler.jar --warning_level QUIET --charset UTF-8 --js lib/sugar.js --js_output_file release/tmp2.js
 cat release/tmp1.js release/tmp2.js > release/sugar-$1.min.js
 rm release/tmp1.js
 rm release/tmp2.js
