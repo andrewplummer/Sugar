7918078d024aea6e8ec748f00626d987d7328424
Andrew Plummer <plummer.andrew@gmail.com>
2011-10-20 00:24:51 +0900

    minor optimizations and modifications. utf8 release
    Former-commit-id: 7c4b9c5045fe431b6f58221f842fdc44ab1899ed [formerly 9333b19ec6d12c1ba154b0f3228c7fdc6b421575]
    Former-commit-id: a5df348196a50e43a7529c0a1106a2472d6f862d

--- script/create_release.sh
+++ script/create_release.sh
@@ -1,5 +1,3 @@
-sed "s/VERSION/$1/" release/copyright.txt > release/tmp1.js
-java -jar script/jsmin/compiler.jar --warning_level QUIET --charset UTF-8 --js lib/sugar.js --js_output_file release/tmp2.js
-cat release/tmp1.js release/tmp2.js > release/sugar-$1.min.js
-rm release/tmp1.js
-rm release/tmp2.js
+#java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js | cat release/copyright.txt - | sed "s/VERSION/$1/" > release/sugar-$1.min.js
+java -jar script/jsmin/compiler.jar --warning_level QUIET --charset UTF-8 --js lib/sugar.js | cat release/copyright.txt - | sed "s/VERSION/$1/" | sed "s/  /\\\u2028\\\u2029/" > release/utf8/sugar-$1.min.js
+
