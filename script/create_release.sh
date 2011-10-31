0c1829b5ddf7f0aaeac93ebadefaa1d8cf68efc2
Andrew Plummer <plummer.andrew@gmail.com>
2011-11-01 03:25:02 +0900

    starting in on optimizations for GCC

--- script/create_release.sh
+++ script/create_release.sh
@@ -1,3 +1,4 @@
-java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js | cat release/copyright.txt - | sed "s/VERSION/$1/" > release/sugar-$1.min.js
-java -jar script/jsmin/compiler.jar --warning_level QUIET --charset UTF-8 --js lib/sugar.js | cat release/copyright.txt - | sed "s/VERSION/$1/" | sed "s/  /\\\u2028\\\u2029/" > release/utf8/sugar-$1.min.js
+java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js --compilation_level ADVANCED_OPTIMIZATIONS --externs script/externs.js | cat release/copyright.txt - | sed "s/VERSION/$1/" > release/sugar-$1.min.js
+#java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js | cat release/copyright.txt - | sed "s/VERSION/$1/" > release/sugar-$1.min.js
+#java -jar script/jsmin/compiler.jar --warning_level QUIET --charset UTF-8 --js lib/sugar.js | cat release/copyright.txt - | sed "s/VERSION/$1/" | sed "s/  /\\\u2028\\\u2029/" > release/utf8/sugar-$1.min.js
 
---
+++ script/externs.js
@@ -0,0 +1,15 @@
+Number.prototype.upto = function(){};
+Number.prototype.chr = function() {};
+String.prototype.shift = function() {};
+String.prototype.each = function() {};
+String.prototype.codes = function() {};
+Object.isFunction = function() {};
+Object.isNumber = function() {};
+Object.isRegExp = function() {};
+Object.isArray = function() {};
+Object.extended = function() {};
+Object.merge = function() {};
+Array.groupBy = function() {};
+
+Date.setLocale = function() {};
+Date.addFormat = function() {};
