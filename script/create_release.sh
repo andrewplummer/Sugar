26a7df9d3fc7e0b23255cc4da4dbf8913a6efdfd
Dmytrii Nagirniak <dnagir@gmail.com>
2011-11-17 13:25:33 +1100

    #55 - The development version is not the same as release. I need to include it into other package that I currently work on for distribution and can't afford to use the master branch for that.
    So just another step in the would be good. It doesn't take much really.
    And most of libs out there do have 2 versions - one minified, the other not.
    
    So something like this would be nice (PLS DOUBLE CHECK THE SCRIPT).
    The step I c

--- script/create_release.sh
+++ script/create_release.sh
@@ -1,2 +1,4 @@
 java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js | cat release/copyright.txt - | sed "s/VERSION/$1/" > release/sugar-$1.min.js
 java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --charset UTF-8 | cat release/copyright.txt - | sed "s/VERSION/$1/" | sed "s/  /\\\u2028\\\u2029/" > release/utf8/sugar-$1.min.js
+
+cat release/copyright.txt lib/externs.js lib/sugar.js | sed "s/VERSION/$1/" > release/sugar-$1.debug.js
