357eae134cd25c04a7c11fd81004917ea0d7226c
Andrew Plummer <plummer.andrew@gmail.com>
2011-11-17 21:59:51 +0900

    Not minified so can't be .min... duh..
    Former-commit-id: 50f009b365be0faf86f9462a14cd3764e1a428f9 [formerly 62ceb293c825a79e2a736e756019edcd40e7357e]
    Former-commit-id: 6767f1ff9d047a0703972fd74f10919b2b940ab9

--- script/create_release.sh
+++ script/create_release.sh
@@ -1,3 +1,3 @@
 java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js | cat release/copyright.txt - | sed "s/VERSION/$1/" > release/sugar-$1.min.js
 java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --charset UTF-8 | cat release/copyright.txt - | sed "s/VERSION/$1/" | sed "s/  /\\\u2028\\\u2029/" > release/utf8/sugar-$1.min.js
-cat release/copyright.txt lib/sugar.js | sed "s/VERSION/$1 (Development)/" > release/sugar-development.min.js
+cat release/copyright.txt lib/sugar.js | sed "s/VERSION/$1 (Development)/" > release/sugar-development.js
