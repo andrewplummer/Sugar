c3d69c94480bc0a3e49592714a34f025b26683f8
Andrew Plummer <plummer.andrew@gmail.com>
2011-12-13 21:11:19 +0900

    reworked the release script to create the closure properly. also refactored Array#sortBy to use .compare

--- script/create_release.sh
+++ script/create_release.sh
@@ -1,3 +1,6 @@
-java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --output_wrapper "(function(){%output%})();" | cat release/copyright.txt - | sed "s/VERSION/$1/" > release/sugar-$1.min.js
-java -jar script/jsmin/compiler.jar --warning_level QUIET --js lib/sugar.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --charset UTF-8 --output_wrapper "(function(){%output%})();" | cat release/copyright.txt - | sed "s/VERSION/$1/" | sed "s/  /\\\u2028\\\u2029/" > release/utf8/sugar-$1.min.js
+S=`awk 'END {print NR-0}' lib/sugar.js`; sed -e '1,2 d' -e "$S,$ d" lib/sugar.js > release/tmp.js
+
+java -jar script/jsmin/compiler.jar --warning_level QUIET --js release/tmp.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --output_wrapper "(function(){%output%})();" | cat release/copyright.txt - | sed "s/VERSION/$1/" > release/sugar-$1.min.js
+java -jar script/jsmin/compiler.jar --warning_level QUIET --js release/tmp.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --charset UTF-8 --output_wrapper "(function(){%output%})();" | cat release/copyright.txt - | sed "s/VERSION/$1/" | sed "s/  /\\\u2028\\\u2029/" > release/utf8/sugar-$1.min.js
 cat release/copyright.txt lib/sugar.js | sed "s/VERSION/$1 (Development)/" > release/sugar-$1.development.js
+rm release/tmp.js
