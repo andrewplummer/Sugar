5eb4c3a1619c6147ec3d29ef797d97eaed377b64
Andrew Plummer <plummer.andrew@gmail.com>
2012-01-14 16:49:49 +0900

    moving release scripts
    Former-commit-id: 4f687172ccb884a19881debdf639a027fe1f4521 [formerly 00bcaea1b768db2367dfa00a161c15fdb695ad14]
    Former-commit-id: 6d64f4a8f185dab39985ad6afa1099d023148fac

--- script/create_release.sh
+++ script/create_release.sh
@@ -1,9 +1,29 @@
-S=`awk 'END {print NR-0}' lib/sugar.js`; sed -e '1,2 d' -e "$S,$ d" lib/sugar.js > release/tmp.js
+# Delete the first and last lines to prevent an extra closure being added by GCC
 
-java -jar script/jsmin/compiler.jar --warning_level QUIET --js release/tmp.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --output_wrapper "(function(){%output%})();" | cat release/copyright.txt - | sed "s/VERSION/$1/" > release/sugar-$1.min.js
+release=$1
 
-java -jar script/jsmin/compiler.jar --warning_level QUIET --js release/tmp.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --charset UTF-8 --output_wrapper "(function(){%output%})();" | cat release/copyright.txt - | sed "s/VERSION/$1/" | sed "s/  /\\\u2028\\\u2029/" > release/utf8/sugar-$1.min.js
+strip_first_and_last_lines()
+{
+  last=`awk 'END {print NR-0}' $1`
+  sed -e '1,2 d' -e "$last,$ d" $1 > release/tmp.js
+}
 
-cat release/copyright.txt lib/sugar.js | sed "s/VERSION/$1 (Development)/" > release/development/sugar-$1.development.js
+gcc_compile()
+{
+  if $1; then
+    utf8="--charset UTF-8"
+  else
+    utf8=""
+  fi
+  `java -jar script/jsmin/compiler.jar --warning_level QUIET --js release/tmp.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js $utf8 --output_wrapper "(function(){%output%})();" --js_output_file release/tmp.js`
+  if $1; then
+    sed "s/VERSION/$release/" release/tmp.js > release/tmp.js
+  fi
+  #| cat release/copyright.txt - | sed "s/VERSION/$release/" > release/sugar-$release.min.js`
+}
 
-rm release/tmp.js
+#java -jar script/jsmin/compiler.jar --warning_level QUIET --js release/tmp.js --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --charset UTF-8 --output_wrapper "(function(){%output%})();" | cat release/copyright.txt - | sed "s/VERSION/$1/" | sed "s/  /\\\u2028\\\u2029/" > release/utf8/sugar-$1.min.js
+
+#cat release/copyright.txt lib/sugar.js | sed "s/VERSION/$1 (Development)/" > release/development/sugar-$1.development.js
+
+#rm release/tmp.js
