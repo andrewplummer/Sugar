87d28040cf5b700c6a9cf6d4371662a55805f5ed
Andrew Plummer <plummer.andrew@gmail.com>
2012-01-15 18:08:18 +0900

    updating release script

--- script/create_release.sh
+++ script/create_release.sh
@@ -1,34 +1,47 @@
+#!/bin/bash
 
-ver=$1
+DIR=`dirname $0`
+VERSION=$1
 
-gcc_compile()
-{
-  if $2; then
-    utf8="--charset UTF-8"
-  else
-    utf8=""
-  fi
-  # Delete the first and last lines to prevent an extra closure being added by GCC
-  last=`awk 'END {print NR-0}' $1`
-  sed -e '1,2 d' -e "$last,$ d" $1 | java -jar script/jsmin/compiler.jar --warning_level QUIET --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js $utf8 --output_wrapper "(function(){%output%})();" | cat release/copyright.txt - | sed "s/VERSION/$ver/" > tmp/compiled.js
-}
+source $DIR/gcc_compile.sh
 
 create_minified_script()
 {
   file="lib/$1.js"
   gcc_compile $file false
-  mv tmp/compiled.js release/$ver/minified/sugar-$ver-$1.min.js
-  cp $file release/$ver/development/sugar-$ver-$1.development.js
+  mv tmp/compiled.js release/$VERSION/minified/sugar-$VERSION-$1.min.js
+  cp $file release/$VERSION/development/sugar-$VERSION-$1.development.js
 }
 
-mkdir release/$ver
-mkdir release/$ver/minified
-mkdir release/$ver/development
+fullname()
+{
+  path="release/$VERSION/$1/sugar-$VERSION"
+  if [ $2 ]; then
+    path="${path}-$2"
+  fi
+  if [ "$1" = "minified" ]; then
+    path="${path}.min.js"
+  else
+    path="${path}.development.js"
+  fi
+  echo $path
+}
+
+strip_license()
+{
+  path=$(fullname $1 $2)
+  echo "`sed "1,8 d" $path`"
+}
+
+mkdir release/$VERSION
+mkdir release/$VERSION/minified
+mkdir release/$VERSION/development
 
 create_minified_script 'core'
 create_minified_script 'dates'
 create_minified_script 'inflections'
 
-cat release/$ver/minified/sugar-$ver-core.min.js release/$ver/minified/sugar-$ver-dates.min.js > release/$ver/minified/sugar-$ver.min.js
-cat release/$ver/development/sugar-$ver-core.development.js release/$ver/development/sugar-$ver-dates.development.js > release/$ver/development/sugar-$ver.development.js
+
+strip_license "minified" "dates" | cat $(fullname "minified" "core") - > $(fullname "minified")
+cat $(fullname "development" "core") $(fullname "development" "dates") > $(fullname "development")
 
