9e82fad0a646e938efa92e84ae96d60349b7fb3a
Andrew Plummer <plummer.andrew@gmail.com>
2012-01-16 23:21:22 +0900

    updated externs and turned tests toward packages...gearing up for release

--- script/gcc_compile.sh
+++ script/gcc_compile.sh
@@ -9,6 +9,6 @@ gcc_compile()
   fi
   # Delete the first and last lines to prevent an extra closure being added by GCC
   last=`awk 'END {print NR-0}' $1`
-  sed -e '1,2 d' -e "$last,$ d" $1 | java -jar script/jsmin/compiler.jar --warning_level QUIET --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js $utf8 --output_wrapper "(function(){%output%})();" | cat release/copyright.txt - | sed "s/VERSION/$VERSION/" > tmp/compiled.js
+  sed -e '1,2 d' -e "$last,$ d" $1 | java -jar script/jsmin/compiler.jar --warning_level QUIET --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js $utf8 --output_wrapper "(function(context){%output%})(this);" | cat release/copyright.txt - | sed "s/VERSION/$VERSION/" > tmp/compiled.js
 }
 
