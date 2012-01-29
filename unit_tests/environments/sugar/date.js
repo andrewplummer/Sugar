35f01412cd2df303848c0ac3f985b06e138dd5bf
Andrew Plummer <plummer.andrew@gmail.com>
2012-01-30 01:10:32 +0900

    simple test fix for timezones

--- unit_tests/environments/sugar/date.js
+++ unit_tests/environments/sugar/date.js
@@ -879,7 +879,7 @@ test('Date', function () {
   equal(d.format('{ord}'), '5th', 'Date#format | custom formats | ord');
 
   var offsetHours = d.getUTCOffset().replace(/(\d{2})\d{2}/, '$1');
-  equal(d.format('{z}'), offsetHours.replace(/0/, '').toString(), 'Date#format | custom formats | z');
+  equal(d.format('{z}'), offsetHours.replace(/0+([1-9])/, '$1').toString(), 'Date#format | custom formats | z');
   equal(d.format('{zz}'), offsetHours, 'Date#format | custom formats | zz');
   equal(d.format('{zzz}'), d.getUTCOffset(), 'Date#format | custom formats | zzz');
   equal(d.format('{tz}'), d.getUTCOffset(), 'Date#format | custom formats | tz');
