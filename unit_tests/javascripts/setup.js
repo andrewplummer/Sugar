874ac12f3182dc6bfb47051f00a92404347d53b3
Andrew Plummer <plummer.andrew@gmail.com>
2012-02-04 03:22:12 +0900

    moving Object.merge back into a self-contained method and added a tentative param "deep". Also drastically optimized performance and added new tests in the process. Also removed isDefined and isNull as they aren't very used and have a performance cost.
    Former-commit-id: c21f799316cdfb43ec794e46c5cc8ee68fca930a [formerly 34c8a0647fd82c2a290d906f670bbb8e13b1ce35]
    Former-commit-id: 51f3ab9a078fd7d8e1dfe71a3e0429ceaa6dd831

--- unit_tests/javascripts/setup.js
+++ unit_tests/javascripts/setup.js
@@ -259,3 +259,21 @@ syncTestsFinished = function() {
 async = function(fn) {
   setTimeout(fn, 200);
 }
+
+runPerformanceTest = function() {
+  var iterations, fn, start, i = 0;
+  if(arguments.length == 1) {
+    iterations = 10000;
+    fn = arguments[0];
+  } else {
+    iterations = arguments[0];
+    fn = arguments[1];
+  }
+  start = new Date();
+  while(i < iterations) {
+    fn();
+    i++;
+  }
+  console.info(iterations, ' iterations finished in ', new Date() - start, ' milliseconds');
+}
+
