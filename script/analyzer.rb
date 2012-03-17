1548a6c76e27ce010c76a31a991c10299739215d
Andrew Plummer <plummer.andrew@gmail.com>
2012-03-18 06:18:05 +0900

    final analyzer output
    Former-commit-id: 1d269325833c4a65d89fddb17c5120eee5715e58 [formerly 451a3bf2dd9148553ca5f10313fea5213a9414a7]
    Former-commit-id: e868b1e992bdd86e697fe559f3885b4e46156a04

---
+++ script/analyzer.rb
@@ -0,0 +1,19 @@
+
+
+analyzer_file = 'lib/analyzer.js'
+
+
+prototype  = File.open('docs/libs/prototype.js', 'r').read
+underscore = File.open('docs/libs/underscore.js', 'r').read
+
+analyzer  = File.open(analyzer_file, 'r').read
+output = prototype + underscore
+
+
+analyzer.gsub!(/(BEGIN LIBS\n).*?(  \/\/ END LIBS)/m, "\\1#{output}\n\\2")
+
+
+
+File.open(analyzer_file, 'w') do |f|
+  f.puts analyzer
+end
