cbe903bdbf73b81566934852dfc5e24efe51b980
Thomas Nadin <nadinengland@gmail.com>
2012-04-02 21:41:51 +0100

    Array#punctuate handles primitive localization

--- release/edge/development/sugar-edge.development.js
+++ release/edge/development/sugar-edge.development.js
@@ -1867,42 +1867,43 @@
       return arguments.length > 0 ? result : result[0];
     },
 
-			/***
-	     * @method punctuate([fn(<n>)], [useOnly] = true)
-	     * @returns String
-	     * @short Builds a grammatical list from the array
-	     * @extra [fn] will be called on every object in the array. The default handler will be used if [fn] is not specified or is false. Supplying [useOnly] will omit 'only' for single length arrays.
-	     * @example
-	     *
-	     *   ['a', 'b', 'c'].punctuate() 							-> 'a, b and c';
-	     *   ['a'].punctuate()					   						-> 'a only';
-	     *   ['a', 2, {c:3}].punctuate()					 		-> 'a, 2 and [object Object]';
-			 *	 ['a', 'b', 'c'].punctuate(function(n) {
-			 *		// returns 'aa, bb and cc'
-			 *   	return n.repeat(2);
-			 *	 })
-			 *	 ['a'].punctuate(false, false)						-> 'a';
-	     *
-	     ***/
-			'punctuate': function(handler, useOnly) {
-			  var sentence = "";
+		/***
+     * @method punctuate([fn(<n>)], [conjunction] = true [useOnly] = true)
+     * @returns String
+     * @short Builds a grammatical list from the array
+     * @extra [fn] will be called on every object in the array. The default handler will be used if [fn] is not specified or is false. Supplying [useOnly] will omit 'only' for single length arrays. Use a custom [conjuction] for localization.
+     * @example
+     *
+     *   ['a', 'b', 'c'].punctuate() 							-> 'a, b and c';
+     *   ['a'].punctuate()					   						-> 'a only';
+     *   ['a', 2, {c:3}].punctuate()					 		-> 'a, 2 and [object Object]';
+		 *	 ['a', 'b', 'c'].punctuate(function(n) {
+		 *		// returns 'aa, bb and cc'
+		 *   	return n.repeat(2);
+		 *	 })
+		 * 	 ['a', 'b', 'c'].punctuate(false, 'et')		-> 'a, b, et c';
+		 *	 ['a'].punctuate(false, false, false)			-> 'a';
+     *
+     ***/
+		'punctuate': function(handler, conjunction, useOnly) {
+		  var sentence = "";
 
-			  if (Object.isFunction(handler) == false || handler === false)
-			    handler = function(n) { return String(n); };
+		  if (Object.isFunction(handler) == false || handler === false)
+		    handler = function(n) { return Object.isString(n) ? n : String(n); };
 
-			  if (Object.isBoolean(useOnly) == false)
-			    useOnly = true;
+			if (Object.isString(conjunction) == false || conjunction === false) conjunction = "and";
+		  if (Object.isBoolean(useOnly) == false) useOnly = true;
 
-			  for (var i = 0; i < this.length; i++) {
-			      sentence += handler(this[i]);
+		  for (var i = 0; i < this.length; i++) {
+		      sentence += handler(this[i]);
 
-			      if (useOnly == true && this.length == 1) sentence += ' only';
-			      else if (i < (this.length - 2)) sentence += ', ';
-			      else if (i == (this.length - 2)) sentence += ' and ';
-			  }
+		      if (useOnly == true && this.length == 1) sentence += ' only';
+		      else if (i < (this.length - 2)) sentence += ', ';
+		      else if (i == (this.length - 2)) sentence += ' ' + conjunction + ' ';
+		  }
 
-			  return sentence;
-			}
+		  return sentence;
+		}
 
   });
 
