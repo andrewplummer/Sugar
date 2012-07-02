5fbcdcc67df205fa7e28f144a772fe50f82592a3
Andrew Plummer <plummer.andrew@gmail.com>
2012-07-02 21:41:04 +0900

    starting on inflections

--- unit_tests/environments/sugar/inflections.js
+++ unit_tests/environments/sugar/inflections.js
@@ -325,70 +325,6 @@ test('Inflections', function () {
     "underground"     : "Underground"
   }
 
-  var OrdinalNumbers = {
-    "-1"    : "-1st",
-    "-2"    : "-2nd",
-    "-3"    : "-3rd",
-    "-4"    : "-4th",
-    "-5"    : "-5th",
-    "-6"    : "-6th",
-    "-7"    : "-7th",
-    "-8"    : "-8th",
-    "-9"    : "-9th",
-    "-10"   : "-10th",
-    "-11"   : "-11th",
-    "-12"   : "-12th",
-    "-13"   : "-13th",
-    "-14"   : "-14th",
-    "-20"   : "-20th",
-    "-21"   : "-21st",
-    "-22"   : "-22nd",
-    "-23"   : "-23rd",
-    "-24"   : "-24th",
-    "-100"  : "-100th",
-    "-101"  : "-101st",
-    "-102"  : "-102nd",
-    "-103"  : "-103rd",
-    "-104"  : "-104th",
-    "-110"  : "-110th",
-    "-111"  : "-111th",
-    "-112"  : "-112th",
-    "-113"  : "-113th",
-    "-1000" : "-1000th",
-    "-1001" : "-1001st",
-    "0"     : "0th",
-    "1"     : "1st",
-    "2"     : "2nd",
-    "3"     : "3rd",
-    "4"     : "4th",
-    "5"     : "5th",
-    "6"     : "6th",
-    "7"     : "7th",
-    "8"     : "8th",
-    "9"     : "9th",
-    "10"    : "10th",
-    "11"    : "11th",
-    "12"    : "12th",
-    "13"    : "13th",
-    "14"    : "14th",
-    "20"    : "20th",
-    "21"    : "21st",
-    "22"    : "22nd",
-    "23"    : "23rd",
-    "24"    : "24th",
-    "100"   : "100th",
-    "101"   : "101st",
-    "102"   : "102nd",
-    "103"   : "103rd",
-    "104"   : "104th",
-    "110"   : "110th",
-    "111"   : "111th",
-    "112"   : "112th",
-    "113"   : "113th",
-    "1000"  : "1000th",
-    "1001"  : "1001st"
-  }
-
   var UnderscoresToDashes = {
     "street"                : "street",
     "street_address"        : "street-address",
@@ -648,11 +584,6 @@ test('Inflections', function () {
   equal('Foo.Bar.Dumb'.namespace(), undefined, 'String#namespace | non-existant bottom')
 
 
-
-  // Test ordinalize
-  Object.each(OrdinalNumbers, function(str, ordinalized) {
-      equal(str.toNumber().ordinalize(), ordinalized, 'String#ordinalize')
-  });
 
   // Test dasherize
   Object.each(UnderscoresToDashes, function(under, dasherized) {
--- unit_tests/environments/sugar/number.js
+++ unit_tests/environments/sugar/number.js
@@ -239,6 +239,86 @@ test('Number', function () {
   equal((-1).ordinalize(), '-1st', 'Number#ordinalize | -1');
 
 
+  // Moved from inflections tests
+
+  var OrdinalNumbers = {
+    "-1"    : "-1st",
+    "-2"    : "-2nd",
+    "-3"    : "-3rd",
+    "-4"    : "-4th",
+    "-5"    : "-5th",
+    "-6"    : "-6th",
+    "-7"    : "-7th",
+    "-8"    : "-8th",
+    "-9"    : "-9th",
+    "-10"   : "-10th",
+    "-11"   : "-11th",
+    "-12"   : "-12th",
+    "-13"   : "-13th",
+    "-14"   : "-14th",
+    "-20"   : "-20th",
+    "-21"   : "-21st",
+    "-22"   : "-22nd",
+    "-23"   : "-23rd",
+    "-24"   : "-24th",
+    "-100"  : "-100th",
+    "-101"  : "-101st",
+    "-102"  : "-102nd",
+    "-103"  : "-103rd",
+    "-104"  : "-104th",
+    "-110"  : "-110th",
+    "-111"  : "-111th",
+    "-112"  : "-112th",
+    "-113"  : "-113th",
+    "-1000" : "-1000th",
+    "-1001" : "-1001st",
+    "0"     : "0th",
+    "1"     : "1st",
+    "2"     : "2nd",
+    "3"     : "3rd",
+    "4"     : "4th",
+    "5"     : "5th",
+    "6"     : "6th",
+    "7"     : "7th",
+    "8"     : "8th",
+    "9"     : "9th",
+    "10"    : "10th",
+    "11"    : "11th",
+    "12"    : "12th",
+    "13"    : "13th",
+    "14"    : "14th",
+    "20"    : "20th",
+    "21"    : "21st",
+    "22"    : "22nd",
+    "23"    : "23rd",
+    "24"    : "24th",
+    "100"   : "100th",
+    "101"   : "101st",
+    "102"   : "102nd",
+    "103"   : "103rd",
+    "104"   : "104th",
+    "110"   : "110th",
+    "111"   : "111th",
+    "112"   : "112th",
+    "113"   : "113th",
+    "1000"  : "1000th",
+    "1001"  : "1001st"
+  }
+
+  // Test ordinalize
+  Object.each(OrdinalNumbers, function(str, ordinalized) {
+      equal(str.toNumber().ordinalize(), ordinalized, 'String#ordinalize')
+  });
+
+
+
+
+
+
+
+
+
+
   equal((100).format(), '100', 'Number#format | 100')
   equal((1).format(), '1', 'Number#format | 1')
   equal((10).format(), '10', 'Number#format | 10')
