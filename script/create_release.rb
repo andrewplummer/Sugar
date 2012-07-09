c1d33d61e58ea8a3049d0406c243a4517509e857
Andrew Plummer <plummer.andrew@gmail.com>
2012-07-10 01:07:52 +0900

    new ruby deploy script
    Former-commit-id: 6da6acc45520d36cd806c285c203e9956839a164 [formerly 16c370c731515dc9fed5761bfa5e9146bcb2c9d5]
    Former-commit-id: 8f804592588979f8343142aad78570dbc3852483

--- script/create_release.rb
+++ script/create_release.rb
@@ -1,6 +1,11 @@
 
+require 'pp'
+
 @version  = ARGV[0]
 @packages = ['core','es5','array','date','date_ranges','function','number','object','regexp','string','inflections']
+@default_package = @packages.values_at(0,1,2,3,5,6,7,8,9)
+@delimiter = 'console.info("-----BREAK-----");'
+@full_path = "release/#{@version}"
 
 if !@version
   puts "No version specified!"
@@ -13,80 +18,51 @@ end
 `mkdir release/#{@version}/precompiled`
 
 
+def concat
+  `rm tmp/uncompiled.js`
+  file = File.open('tmp/uncompiled.js', 'w')
+  @packages.each do |p|
+    file.puts content = File.open("lib/#{p}.js").read + @delimiter
+  end
+end
+
 def compile
-  command = "java -jar script/jsmin/compiler.jar --warning_level QUIET --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --variable_map_output_file tmp/variable_map --property_map_output_file tmp/property_map #{@packages.map { |p| " --js lib/" + p + ".js" }} --js_output_file tmp/compiled.js"
+  command = "java -jar script/jsmin/compiler.jar --warning_level QUIET --compilation_level ADVANCED_OPTIMIZATIONS --externs lib/externs.js --variable_map_output_file tmp/variable_map --property_map_output_file tmp/property_map --js tmp/uncompiled.js --js_output_file tmp/compiled.js"
   puts "EXECUTING: #{command}"
   `#{command}`
 end
 
 def split_compiled
-  file = 'tmp/compiled.js'
-  contents = File.open(file, 'r').read.split('console.info("-----BREAK-----");')
+  contents = File.open('tmp/compiled.js', 'r').read.split('console.info("-----BREAK-----");')
   @packages.each_with_index do |name, index|
-    File.open("tmp/#{name}", 'w') do |f|
-      puts contents[index]
+    File.open("#{@full_path}/precompiled/#{name}.js", 'w') do |f|
       f.puts contents[index]
     end
   end
 end
 
-#File.open('/tmp/ruby-write-test.txt', 'w') do |f|
-#2.
-#f.puts 'test started at #{Time.now}'
-#3.
-## ... do something
-#4.
-#f.puts 'test ended at #{Time.now}'
-#5.
+def create_packages
+  create_package('default', @default_package)
+end
 
+def create_package(name, arr)
+  contents = ''
+  arr.each do |s|
+    contents << File.open("#{@full_path}/precompiled/#{s}.js").read
+  end
+  with_wrapper = "(function(){#{contents.sub(/\n+\Z/m, '')}})();"
+  File.open("#{@full_path}/minified/sugar-#{@version}-#{name}.min.js", 'w').write(with_wrapper)
+end
 
-#end
+def cleanup
+  `rm tmp/variable_map`
+  `rm tmp/property_map`
+  `rm tmp/compiled`
+  `rm tmp/uncompiled`
+end
 
+concat
 compile
 split_compiled
-
-#DIR=`dirname $0`
-#VERSION=$1
-
-#source $DIR/gcc_compile.sh
-
-#create_minified_script()
-#{
-  #file="lib/$1.js"
-  #gcc_compile $file false "tmp/variable_map" "tmp/property_map"
-  ##mv tmp/compiled.js release/$VERSION/minified/sugar-$VERSION-$1.min.js
-  ##cp $file release/$VERSION/development/sugar-$VERSION-$1.development.js
-#}
-
-#fullname()
-#{
-  #path="release/$VERSION/$1/sugar-$VERSION"
-  #if [ $2 ]; then
-    #path="${path}-$2"
-  #fi
-  #if [ "$1" = "minified" ]; then
-    #path="${path}.min.js"
-  #else
-    #path="${path}.development.js"
-  #fi
-  #echo $path
-#}
-
-#strip_license()
-#{
-  #path=$(fullname $1 $2)
-  #echo "`sed "1,8 d" $path`"
-#}
-
-#create_output_map
-
-
-##create_minified_script 'core'
-##create_minified_script 'dates'
-##create_minified_script 'inflections'
-##create_minified_script 'dates-only'
-
-
-##strip_license "minified" "dates" | cat $(fullname "minified" "core") - > $(fullname "minified")
-##cat $(fullname "development" "core") $(fullname "development" "dates") > $(fullname "development")
+create_packages
 
