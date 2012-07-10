d98a6d195ead3d2ae8fa248cfbd11a38e1b3cd1a
Andrew Plummer <plummer.andrew@gmail.com>
2012-07-11 01:11:54 +0900

    deploy for last fix

--- release/edge/precompiled/regexp.js
+++ release/edge/precompiled/regexp.js
@@ -1,2 +1,2 @@
-r.oa=E(r("()??").exec("")[1]);y(r,m,m,{escape:function(a){return S(a)}});
-y(r,j,m,{getFlags:function(){return va(this)},setFlags:function(a){return r(this.source,a)},addFlag:function(a){return this.setFlags(va(this,a))},removeFlag:function(a){return this.setFlags(va(this).replace(a,""))}});
+s.oa=G(s("()??").exec("")[1]);x(s,m,m,{escape:function(a){return S(a)}});
+x(s,j,m,{getFlags:function(){return va(this)},setFlags:function(a){return s(this.source,a)},addFlag:function(a){return this.setFlags(va(this,a))},removeFlag:function(a){return this.setFlags(va(this).replace(a,""))}});
