8c3d40ddf48313189752084bbad4698918ba5967
Andrew Plummer <plummer.andrew@gmail.com>
2012-07-10 23:42:13 +0900

    trying out optimizations but not really that impressive
    Former-commit-id: d9f0eb74c1dce444db43e865e26a38d79e80ea02 [formerly 74ff4940d9163439bdd645a0d3ded130f2b910d9]
    Former-commit-id: 50cb4460861ec26779b0e083ff27ebdcb5705c6a

--- release/edge/precompiled/regexp.js
+++ release/edge/precompiled/regexp.js
@@ -1,2 +1,2 @@
-q.oa=E(q("()??").exec("")[1]);y(q,m,m,{escape:function(a){return S(a)}});
-y(q,j,m,{getFlags:function(){return ua(this)},setFlags:function(a){return q(this.source,a)},addFlag:function(a){return this.setFlags(ua(this,a))},removeFlag:function(a){return this.setFlags(ua(this).replace(a,""))}});
+r.oa=E(r("()??").exec("")[1]);y(r,m,m,{escape:function(a){return S(a)}});
+y(r,j,m,{getFlags:function(){return va(this)},setFlags:function(a){return r(this.source,a)},addFlag:function(a){return this.setFlags(va(this,a))},removeFlag:function(a){return this.setFlags(va(this).replace(a,""))}});
