001af4a75f330d5a13a894cf98307c62d55a29e2
Andrew Plummer <plummer.andrew@gmail.com>
2012-08-07 01:34:40 +0900

    more robust abbreviations for locales

--- release/edge/precompiled/minified/regexp.js
+++ release/edge/precompiled/minified/regexp.js
@@ -1,2 +1,2 @@
 D(r,m,m,{escape:function(a){return R(a)}});
-D(r,j,m,{getFlags:function(){return ra(this)},setFlags:function(a){return r(this.source,a)},addFlag:function(a){return this.setFlags(ra(this,a))},removeFlag:function(a){return this.setFlags(ra(this).replace(a,""))}});
+D(r,k,m,{getFlags:function(){return ra(this)},setFlags:function(a){return r(this.source,a)},addFlag:function(a){return this.setFlags(ra(this,a))},removeFlag:function(a){return this.setFlags(ra(this).replace(a,""))}});
