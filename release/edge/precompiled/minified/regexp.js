004bf25aa752b7373fa7f2d205f7718c48adc069
Andrew Plummer <plummer.andrew@gmail.com>
2012-09-30 22:19:03 +0900

    Date.future failure fix (Issue #210)

--- release/edge/precompiled/minified/regexp.js
+++ release/edge/precompiled/minified/regexp.js
@@ -1,2 +1,2 @@
-E(q,m,m,{escape:function(a){return Q(a)}});
-E(q,k,m,{getFlags:function(){return sa(this)},setFlags:function(a){return q(this.source,a)},addFlag:function(a){return this.setFlags(sa(this,a))},removeFlag:function(a){return this.setFlags(sa(this).replace(a,""))}});
+E(r,n,n,{escape:function(a){return Q(a)}});
+E(r,k,n,{getFlags:function(){return sa(this)},setFlags:function(a){return r(this.source,a)},addFlag:function(a){return this.setFlags(sa(this,a))},removeFlag:function(a){return this.setFlags(sa(this).replace(a,""))}});
