12c1a80c76b1656a76efc6ed13c5c550d3fc57f9
Andrew Plummer <plummer.andrew@gmail.com>
2013-01-08 02:00:08 +0900

    removing old packages

--- release/1.3.6/precompiled/minified/regexp.js
+++
@@ -1,2 +0,0 @@
-E(r,n,n,{escape:function(a){return Q(a)}});
-E(r,k,n,{getFlags:function(){return ta(this)},setFlags:function(a){return r(this.source,a)},addFlag:function(a){return this.setFlags(ta(this,a))},removeFlag:function(a){return this.setFlags(ta(this).replace(a,""))}});
--- release/1.3/precompiled/minified/regexp.js
+++
@@ -1,2 +0,0 @@
-G(s,m,m,{escape:function(a){return R(a)}});
-G(s,j,m,{getFlags:function(){return sa(this)},setFlags:function(a){return s(this.source,a)},addFlag:function(a){return this.setFlags(sa(this,a))},removeFlag:function(a){return this.setFlags(sa(this).replace(a,""))}});
