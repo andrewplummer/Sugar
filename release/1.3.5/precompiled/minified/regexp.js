206c23aa91381534c17a091914cd01f312b94a96
Andrew Plummer <plummer.andrew@gmail.com>
2013-01-08 02:00:08 +0900

    removing old packages
    Former-commit-id: 1f2b856f23ea6374d140dc6646c5e156d9e0a880 [formerly ba8ad61c3a8c2524da240f20fe56e8efddb33ea9]
    Former-commit-id: 7f58c53f8a9ff4ae0a567057e1aea6b1e47dce63

--- release/1.3.5/precompiled/minified/regexp.js
+++
@@ -1,2 +0,0 @@
-E(r,n,n,{escape:function(a){return Q(a)}});
-E(r,k,n,{getFlags:function(){return sa(this)},setFlags:function(a){return r(this.source,a)},addFlag:function(a){return this.setFlags(sa(this,a))},removeFlag:function(a){return this.setFlags(sa(this).replace(a,""))}});
