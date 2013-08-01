bccc33c6aac684f80b0c4dc2bf08ccbee0f494a2
Andrew Plummer <plummer.andrew@gmail.com>
2013-08-01 23:31:50 +0900

    trying to create a better performance measuring mechanism
    Former-commit-id: 2ef433b94c982b3ea741f64ebc603c227dcf6db6 [formerly f8fac6ab5c6749ded2e0cd0a5bad3a914e89e72b]
    Former-commit-id: 9bc41dbd75e4baeb5a6c8b7bd9dda95407f5ea8c

--- performance/javascripts/benchmark.js
+++ performance/javascripts/benchmark.js
@@ -1,6 +1,7 @@
 
 runPerformanceTest = function() {
-  var iterations, fn, start, i = 0;
+  var iterations, fn, start, i = 0, ms;
+  var passedArg = arguments[3];
   if(arguments.length == 1) {
     iterations = 10000;
     fn = arguments[0];
@@ -10,9 +11,57 @@ runPerformanceTest = function() {
   }
   start = new Date();
   while(i < iterations) {
-    fn();
+    fn(passedArg);
     i++;
   }
-  console.info(iterations + ' iterations finished in ' + (new Date() - start) + ' milliseconds');
+  ms = new Date() - start
+  console.info(iterations + ' iterations finished in ' + ms + ' milliseconds');
+  return ms;
 }
 
+function runMultipleTestsWithArgumentAndAverage(test, arg, iterationsEach, times) {
+  var sum = 0;
+  for (var i = 0; i < times; i += 1) {
+    sum += runPerformanceTest(iterationsEach, test, arg);
+  };
+  return Math.round(sum / times);
+}
+
+$.get('sample.json', function(obj) {
+  jsonArray = obj.items;
+});
+
+normalDate = new Date();
+
+zero           = 0;
+smallInteger   = 85;
+hugeNumber     = 893249283429;
+floatingNumber = 463.34534533;
+
+
+
+emptyString  = '';
+normalString = 'abcdefg';
+hugeString   = '';
+
+
+emptyArray       = [];
+smallNumberArray = [1,2,3];
+smallStringArray = ['a','b','c'];
+
+bigNumberArray = [];
+bigStringArray = [];
+bigDateArray   = [];
+
+(function() {
+
+  for(var i = 0; i < 10000; i++) {
+    var rand = Math.floor(Math.random() * 1000);
+    var char = String.fromCharCode(rand);
+    bigNumberArray.push(rand);
+    bigDateArray.push(new Date(rand));
+    bigStringArray.push(char);
+    hugeString += char;
+  }
+
+})();
