adca02f69530c9bedfd07a38c9ba22f2100ef41a
Andrew Plummer <plummer.andrew@gmail.com>
2014-03-31 23:46:05 +0900

    added Number.isNaN shim

--- test/environments/sugar/es6.js
+++ test/environments/sugar/es6.js
@@ -838,6 +838,28 @@ package('ES6', function () {
   equal(String.prototype.contains.apply({ 'toString': function() { return 'abc'; } }, ['b', 1]), true);
   equal(String.prototype.contains.apply({ 'toString': function() { return 'abc'; } }, ['b', 2]), false);
 
+  // Number.isNaN
+  // Tests from https://github.com/ljharb/is-nan/blob/master/test.js
+
+  equal(Number.isNaN(), false, 'undefined is not NaN');
+  equal(Number.isNaN(null), false, 'null is not NaN');
+  equal(Number.isNaN(false), false, 'false is not NaN');
+  equal(Number.isNaN(true), false, 'true is not NaN');
+  equal(Number.isNaN(0), false, 'positive zero is not NaN');
+  equal(Number.isNaN(Infinity), false, 'Infinity is not NaN');
+  equal(Number.isNaN(-Infinity), false, '-Infinity is not NaN');
+  equal(Number.isNaN('foo'), false, 'string is not NaN');
+  equal(Number.isNaN([]), false, 'array is not NaN');
+  equal(Number.isNaN({}), false, 'object is not NaN');
+  equal(Number.isNaN(function () {}), false, 'function is not NaN');
+  equal(Number.isNaN('NaN'), false, 'string NaN is not NaN');
+
+  var obj = { valueOf: function () { return NaN; } };
+  equal(Number.isNaN(Number(obj)), true, 'object with valueOf of NaN, converted to Number, is NaN');
+  equal(Number.isNaN(obj), false, 'object with valueOf of NaN is not NaN');
+
+  equal(Number.isNaN(NaN), true, 'NaN is NaN');
+
   // End stolen unit tests
 
 });
