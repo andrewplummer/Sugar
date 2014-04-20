4769d6d4f79db084cadee6e6ce74e14d2c6179f0
Andrew Plummer <plummer.andrew@gmail.com>
2014-04-20 18:03:12 +0900

    re-arranging trim/trimLeft/trimRight to reflect their proper place as standard/non-standard methods
    Former-commit-id: 05dc38047ff757368ab016ee5a76698cb7901ab6 [formerly 6d8a6d18da975cb5887732fb1be785ba9f41895c]
    Former-commit-id: 8420bf9812166578e989deb9de491144b5cf1f53

--- test/environments/sugar/string.js
+++ test/environments/sugar/string.js
@@ -161,23 +161,23 @@ package('String', function () {
 
   });
 
-  method('trim', function() {
-    // String#trim is ES5 and should always exist on the prototype.
-    equal('   wasabi   '.trim(), 'wasabi', 'should trim both left and right whitespace');
-    equal(''.trim(), '', 'blank');
-    equal(' wasabi '.trim(), 'wasabi', 'wasabi with whitespace');
-  });
+  var whiteSpace = '\u0009\u000B\u000C\u0020\u00A0\uFEFF\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000';
+  var lineTerminators = '\u000A\u000D\u2028\u2029';
 
   method('trimLeft', function() {
     test('   wasabi   ', 'wasabi   ', 'should trim left whitespace only');
     test('', '', 'blank');
     test(' wasabi ', 'wasabi ', 'wasabi with whitespace');
+    test(whiteSpace, '', 'should trim all WhiteSpace characters defined in 7.2 and Unicode "space, separator"');
+    test(lineTerminators, '', 'should trim all LineTerminator characters defined in 7.3');
   });
 
   method('trimRight', function() {
     test('   wasabi   ', '   wasabi', 'should trim right whitespace only');
     test('', '', 'blank');
     test(' wasabi ', ' wasabi', 'wasabi with whitespace');
+    test(whiteSpace, '', 'should trim all WhiteSpace characters defined in 7.2 and Unicode "space, separator"');
+    test(lineTerminators, '', 'should trim all LineTerminator characters defined in 7.3');
   });
 
   method('pad', function() {
