3cbd756c42fdd3a7ec244e1c4d010fc86f05febb
Andrew Plummer <plummer.andrew@gmail.com>
2015-07-20 12:06:27 +0900

    big rewrite to handle precompiled

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var fs       = require('fs'),
     gulp     = require('gulp'),
+    glob     = require('glob'),
     args     = require('yargs').argv,
     util     = require('gulp-util'),
     merge    = require('merge-stream'),
-    concat   = require('gulp-concat'),
+    concat   = require('gulp-concat-util'),
     replace  = require('gulp-replace'),
-    compiler = require('gulp-closure-compiler');
+    compiler = require('closure-compiler-stream');
 
 var COMPIER_JAR_PATH = 'bower_components/closure-compiler/compiler.jar';
+var PRECOMPILED_MIN_DIR = 'release/precompiled/minified/';
+var PRECOMPILED_DEV_DIR = 'release/precompiled/development/';
 
 var HELP_MESSAGE = [
   '',
@@ -43,7 +46,6 @@ var HELP_MESSAGE = [
   ''
 ].join('\n');
 
-
 var DEFAULT_PACKAGES = [
   'es5',
   'array',
@@ -71,90 +73,48 @@ var ALL_PACKAGES = [
   'locales'
 ];
 
-function outputWithCopyright(packages, ext, fn) {
-  var filename,
-      release = args.r || args.release || 'custom',
-      packages = packages || args.p || args.packages || 'default';
+function getPackages() {
+  return args.p || args.packages || 'default';
+}
+
+function getFiles(packages) {
+  var arr, files = [];
   switch(packages) {
     case 'default':
-      filename = 'sugar' + ext;
-      break;
+      arr = DEFAULT_PACKAGES;
+    break;
     case 'all':
-      filename = 'sugar-full' + ext;
-      break;
+      arr = ALL_PACKAGES;
+    break;
     default:
-      filename = 'sugar-custom' + ext;
+      arr = packages.split(',');
   }
-
-  var copyrightStream = gulp.src('release/copyright.txt')
-    .pipe(replace(/VERSION/, release.match(/[\d.]+/) ? 'v' + release : release))
-    .pipe(replace(/YEAR/, new Date().getFullYear()));
-
-  var fileStream = fn(getFileStream(packages), packages);
-
-  return merge(copyrightStream, fileStream)
-    .pipe(concat(filename, { newLine: '' }))
-    .pipe(gulp.dest('release'));
-}
-
-function getFileStream(packages) {
-  switch(packages) {
-    case 'default':
-      packages = DEFAULT_PACKAGES;
-      break;
-    case 'all':
-      packages = ALL_PACKAGES;
-      break;
-    default:
-      packages = packages.split(',');
-  }
-  return gulp.src(['lib/core/core.js', 'lib/common.js'].concat(packages.map(function(p) {
-    return 'lib/' + (p === 'locales' ? 'locales/*.js' : p.toLowerCase() + '.js');
-  })));
-}
-
-function buildDevelopment(packages) {
-  var wrapper = [
-    '(function() {',
-      "  'use strict';",
-      '$1',
-    '})();'
-  ].join('\n');
-
-  return outputWithCopyright(packages, '.dev.js', function(fileStream, p) {
-    util.log(util.colors.yellow('Concatenating', p));
-    return fileStream
-    .pipe(replace(/'use strict';/g, ''))
-    .pipe(concat('tmp', { newLine: '' }))
-    .pipe(replace(/^([\s\S]+)$/m, wrapper));
+  arr.forEach(function(p) {
+    if (p === 'locales') {
+      files = files.concat(glob.sync('lib/locales/*.js'));
+    } else {
+      files.push('lib/' + p + '.js');
+    }
   });
+  return ['lib/core/core.js', 'lib/common.js'].concat(files);
 }
 
-function buildMinified(packages) {
-  try {
-    fs.lstatSync(COMPIER_JAR_PATH);
-  } catch(e) {
-    util.log(util.colors.red('Closure compiler missing!'), 'Run', util.colors.yellow('bower install'));
-    return;
-  }
-  return outputWithCopyright(packages, '.min.js', function(fileStream, p) {
-    util.log(util.colors.yellow('Minifying', p));
-    return fileStream
-      .pipe(
-        compiler({
-          compilerPath: COMPIER_JAR_PATH,
-          fileName: 'tmp.js',
-          compilerFlags: {
-            compilation_level: 'ADVANCED_OPTIMIZATIONS',
-            externs: [
-              'script/jsmin/externs.js'
-            ],
-            output_wrapper: "(function(){'use strict';%output%}).call(window);",
-            warning_level: 'QUIET'
-          }
-        })
-      );
+function getModules(files) {
+  var modules = [], locales = [];
+  files.forEach(function(f) {
+    var name = f.match(/(\w+)\.js/)[1];
+    if (name === 'core') {
+      modules.push(['core:1', f]);
+    } else if (f.match(/locales/)) {
+      locales.push(f);
+    } else {
+      modules.push([name + ':1:core', f]);
+    }
   });
+  if (locales.length) {
+    modules.push(['locales:' + locales.length + ':core'].concat(locales));
+  }
+  return modules;
 }
 
 function showHelpMessage() {
@@ -172,31 +132,112 @@ function showHelpMessage() {
   console.info(msg);
 }
 
+function getFilename(packages, ext) {
+  switch(packages) {
+    case 'default':
+      return 'sugar.' + ext + '.js';
+    case 'all':
+      return 'sugar-full.' + ext + '.js';
+    default:
+      return 'sugar-custom.' + ext + '.js';
+  }
+}
+
+function getLicense() {
+  var release = args.r || args.release || 'custom';
+  return fs.readFileSync('release/copyright.txt', 'utf-8')
+    .replace(/VERSION/, release.match(/[\d.]+/) ? 'v' + release : release)
+    .replace(/YEAR/, new Date().getFullYear())
+    .replace(/\n$/, '');
+}
+
+function buildDevelopment(packages) {
+  var wrapper = [
+    getLicense(),
+    '(function() {',
+      "  'use strict';",
+      '$1',
+    '})();'
+  ].join('\n');
+  var filename = getFilename(packages, 'dev');
+  var files = getFiles(packages);
+  util.log(util.colors.yellow('Building:', packages));
+  return gulp.src(files)
+    .pipe(concat(filename, { newLine: '' }))
+    .pipe(replace(/'use strict';/g, ''))
+    .pipe(replace(/^([\s\S]+)$/m, wrapper))
+    .pipe(gulp.dest('release'));
+}
+
+function buildMinified(packages) {
+  try {
+    fs.lstatSync(COMPIER_JAR_PATH);
+  } catch(e) {
+    util.log(util.colors.red('Closure compiler missing!'), 'Run', util.colors.yellow('bower install'));
+    return;
+  }
+  var filename = getFilename(packages, 'min');
+  var files = getFiles(packages);
+  util.log(util.colors.yellow('Minifying:', packages));
+  return gulp.src(files).pipe(compileSingle('release/' + filename));
+}
+
+// -------------- Compiler ----------------
+
+function compileModules(modules) {
+  var flags = getDefaultFlags();
+  flags.module = modules;
+  flags.module_output_path_prefix = PRECOMPILED_MIN_DIR;
+  return compiler(flags);
+}
+
+function compileSingle(path) {
+  var flags = getDefaultFlags();
+  flags.js_output_file = path;
+  return compiler(flags);
+}
+
+function getDefaultFlags() {
+  return {
+    jar: COMPIER_JAR_PATH,
+    compilation_level: 'ADVANCED_OPTIMIZATIONS',
+    warning_level: 'QUIET',
+    output_wrapper: getLicense() + "\n(function(){'use strict';%output%}).call(window);",
+    externs: 'script/jsmin/externs.js',
+  }
+}
+
+// -------------- Tasks ----------------
+
 gulp.task('default', showHelpMessage);
 
 gulp.task('help', showHelpMessage);
 
 gulp.task('dev', function() {
-  buildDevelopment();
+  return buildDevelopment(getPackages());
 });
 
-gulp.task('min', function() {
-  buildMinified();
+gulp.task('min', function(done) {
+  return buildMinified(getPackages());
 });
 
-// Multiple minification can't be parallel because the compiler is
-// looking for files on disk, so build them up sequentially here.
-// TODO: Gulp 4 solves this problem.
-
-gulp.task('release:default', ['dev'], function() {
-  return buildMinified('default');
+gulp.task('release', function() {
+  return merge(buildDevelopment('default'), buildMinified('default'), buildDevelopment('all'), buildMinified('all'));
 });
 
-gulp.task('release:dev:all', ['release:default'], function() {
-  return buildDevelopment('all');
+gulp.task('precompile:dev', function() {
+  var files = getFiles('all').filter(function(path) {
+    return !path.match(/locales/);
+  });
+  return merge(gulp.src(files), gulp.src('lib/locales/*.js')
+      .pipe(concat('locales.js', { newLine: '' })))
+    .pipe(replace(/'use strict';/g, ''))
+    .pipe(gulp.dest(PRECOMPILED_DEV_DIR));
 });
 
-gulp.task('release', ['release:dev:all'], function() {
-  return buildMinified('all');
+gulp.task('precompile:min', function() {
+  var files = getFiles('all');
+  var modules = getModules(files);
+  return gulp.src(files).pipe(compileModules(modules));
 });
 
