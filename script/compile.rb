aebac1cb9bfde19337ea88da6353ec77d580db0a
Andrew Plummer <plummer.andrew@gmail.com>
2015-08-04 00:13:46 +0900

    gulpfile now replaces script/
    Former-commit-id: 1c4a9de4c38e374d443d5eef9fb4480ec1ef3dbc [formerly 8acda9bd8bd8d03267e01bf2268f70d8b10b67f6]
    Former-commit-id: 32acbe4cedc2f50cb6353f0f8caf1bec172594cb

--- script/compile.rb
+++
@@ -1,7 +0,0 @@
-
-def compile(input, output)
-  command = "java -jar bower_components/closure-compiler/compiler.jar --warning_level QUIET --compilation_level ADVANCED_OPTIMIZATIONS --externs script/jsmin/externs.js --js #{input} --js_output_file #{output}"
-  puts "EXECUTING: #{command}"
-  `#{command}`
-end
-
