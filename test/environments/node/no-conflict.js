7331f15ba746257ef0ebb8b64dab5f45b1eb0811
Andrew Plummer <plummer.andrew@gmail.com>
2015-08-09 04:22:20 +0900

    fixing revamped unit tests in node
    Former-commit-id: a7497e12f73f93a432049bb77fbe6945c52fc513 [formerly f17e15cadda8ac99eabe9021166a7993982eb549]
    Former-commit-id: ac330d0b0ea547171d8dbea9d57cba1a0618693e

--- test/environments/node/no-conflict.js
+++ test/environments/node/no-conflict.js
@@ -1,20 +1,21 @@
 package('No Conflict', function() {
 
-  process.env.SUGAR_NO_CONFLICT = 'true';
+  xgroup('No Conflict', function() {
+    process.env.SUGAR_NO_CONFLICT = 'true';
+    var Sugar = require( '../../../release/sugar-full.dev');
+    equal(global['Sugar'], undefined, 'Sugar should not exist in the global namespace');
 
-  var Sugar = require( '../../../release/sugar-full.dev');
-  equal(global['Sugar'], undefined, 'Sugar should not exist in the global namespace');
+    Sugar.extend(Date, {
+      foobar: function() {
+        return 'foobar!';
+      }
+    });
 
-  Sugar.extend(Date, {
-    foobar: function() {
-      return 'foobar!';
-    }
+    equal(Sugar.Date.foobar(), 'foobar!', 'Method should be mapped to the required object');
+    equal(new Date().foobar, undefined, 'Method should not be mapped to native prototype');
+
+    delete process.env.SUGAR_NO_CONFLICT;
   });
-
-  equal(Sugar.Date.foobar(), 'foobar!', 'Method should be mapped to the required object');
-  equal(new Date().foobar, undefined, 'Method should not be mapped to native prototype');
-
-  delete process.env.SUGAR_NO_CONFLICT;
 
 });
 
--- test/environments/node/test.js
+++ test/environments/node/test.js
@@ -6,6 +6,8 @@ require('../../javascripts/suite.js');
 require('../../javascripts/helpers.js');
 require('../../javascripts/date-helpers.js');
 
+sinon = require('sinon');
+
 // No conflict tests
 require('./no-conflict.js');
 
@@ -26,7 +28,17 @@ function logResults(runtime, results) {
       failure = results[i].failures[j];
       console.info('\n'+ (j + 1) + ') Failure:');
       console.info(failure.message);
-      console.info('Expected: ' + JSON.stringify(failure.expected) + ' but was: ' + JSON.stringify(failure.actual));
+      try {
+        console.info('Expected: ' + JSON.stringify(failure.expected) + ' but was: ' + JSON.stringify(failure.actual));
+      } catch(e) {
+        if (e instanceof TypeError) {
+          // Sugar.Date has a "toJSON" method which means if it or the Sugar
+          // global is stringified it will error. In most cases it's the global.
+          console.error('Actual value was likely Sugar global object!');
+        } else {
+          throw e;
+        }
+      }
       console.info('File: ' + failure.file + ', Line: ' + failure.line, ' Col: ' + failure.col + '\n');
     }
   };
@@ -36,34 +48,35 @@ function logResults(runtime, results) {
 }
 
 // Tests
-require('../sugar/object.js');
-require('../sugar/string.js');
-require('../sugar/string_range.js');
+
 require('../sugar/array.js');
+require('../sugar/date.js');
+require('../sugar/date_de.js');
+require('../sugar/date_es.js');
+require('../sugar/date_fr.js');
+require('../sugar/date_it.js');
+require('../sugar/date_ja.js');
+require('../sugar/date_ko.js');
+require('../sugar/date_nl.js');
+require('../sugar/date_pt.js');
+require('../sugar/date_ru.js');
+require('../sugar/date_sv.js');
+require('../sugar/date_zh_cn.js');
+require('../sugar/date_zh_tw.js');
+require('../sugar/date_range.js');
 require('../sugar/enumerable.js');
 require('../sugar/equals.js');
-require('../sugar/number.js');
-require('../sugar/number_range.js');
-require('../sugar/regexp.js');
-require('../sugar/function.js');
-require('../sugar/global.js');
 require('../sugar/es5.js');
 require('../sugar/es6.js');
-require('../sugar/date.js');
-require('../sugar/date_range.js');
-require('../sugar/date_de.js')
-require('../sugar/date_es.js')
-require('../sugar/date_fr.js')
-require('../sugar/date_it.js')
-require('../sugar/date_ja.js')
-require('../sugar/date_ko.js')
-require('../sugar/date_nl.js')
-require('../sugar/date_pt.js')
-require('../sugar/date_ru.js')
-require('../sugar/date_sv.js')
-require('../sugar/date_zh_cn.js')
-require('../sugar/date_zh_tw.js')
+require('../sugar/function.js');
+require('../sugar/global.js');
 require('../sugar/inflections.js');
 require('../sugar/language.js');
+require('../sugar/number.js');
+require('../sugar/number_range.js');
+require('../sugar/object.js');
+require('../sugar/regexp.js');
+require('../sugar/string.js');
+require('../sugar/string_range.js');
 
 runTests(logResults);
