2a9daa453878894846415d27c53e3e542a8f256d
Andrew Plummer <plummer.andrew@gmail.com>
2015-09-11 02:40:34 +0900

    adding native tests to get started

---
+++ test/node/default/all.js
@@ -0,0 +1,14 @@
+require('./array.js');
+require('./core.js');
+require('./date.js');
+require('./date-locales.js');
+require('./function.js');
+require('./inflections.js');
+require('./language.js');
+require('./number.js');
+require('./object.js');
+require('./range.js');
+require('./regexp.js');
+require('./string.js');
+require('./sugar.js');
+require('./full.js');
