2a7464ef66f35aea3a2e34c5390cb87abf38711b
Andrew Plummer <plummer.andrew@gmail.com>
2015-10-06 02:30:26 +0900

    Fix for yy format token on zero leading digits. (Issue #498)
    Former-commit-id: 6fdb3d187d932bf8344ea4b74c2834419921f454
    Former-commit-id: c2c3c36ec92f576d90698341fd48554d78ed8380

--- test/tests/date.js
+++ test/tests/date.js
@@ -1440,6 +1440,10 @@ package('Date', function () {
     // Issue #262
     equal(/\d+-/.test(run(new Date(), 'format', ['{timezone}'])), false, 'Timezone format should not include hyphens')
 
+    // Issue #498
+    test(new Date(1901, 0, 2), ['{yy}'], '01', 'Zero padded year should respect yy format');
+    test(new Date(1901, 0, 2), ['{yyyy}'], '1901', 'Zero padded year should respect yyyy format');
+
   });
 
   group('Locale Specific Formats', function() {
