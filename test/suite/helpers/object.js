0ec5b17e66af84ddb7b81f32fdccf5ed4bf564f5
Andrew Plummer <plummer.andrew@gmail.com>
2015-10-25 13:02:31 +0900

    need to use defineProperty for getAccessorObject as get ... is a syntax error in < IE9.

--- test/suite/helpers/object.js
+++ test/suite/helpers/object.js
@@ -1,18 +1,19 @@
-
-
 getAccessorObject = function() {
-  return {
-    data: {
-      label: 'foo'
-    },
-
-    get label() {
+  var obj = {
+    data: { label: 'foo' }
+  }
+  // Need to use defineProperty here as get label() is a syntax error
+  // that would halt tests in < IE9.
+  Object.defineProperty(obj, 'label', {
+    enumerable: true,
+    get: function() {
       return this.data.label;
     },
-    set label(value) {
+    set: function(value) {
       this.data.label = value;
     }
-  };
+  });
+  return obj;
 }
 
 getDescriptorObject = function() {
