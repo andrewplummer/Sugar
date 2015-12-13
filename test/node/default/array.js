7256f5195ad7ed08c8974d5ed5e3bdb74d1e1648
Andrew Plummer <plummer.andrew@gmail.com>
2015-12-13 18:49:40 +0900

    getting all node test packages working!

--- test/node/default/array.js
+++ test/node/default/array.js
@@ -3,6 +3,8 @@ var runner = require('../setup');
 runner.loadPackage('../../release/npm/sugar/array');
 
 // Tests
+runner.loadTest('es5');
+runner.loadTest('es6');
 runner.loadTest('array');
 runner.loadTest('enumerable');
 
