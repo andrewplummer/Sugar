9a6ba8805a5f6059987048947aa0c3cde3e11c23
Andrew Plummer <plummer.andrew@gmail.com>
2016-01-12 04:11:43 +0900

    making tests more readable and understandable. also added a test for checked-in bundle that will become the default test

--- test/node/all.js
+++ test/node/all.js
@@ -1,40 +1,42 @@
 var runner = require('./setup');
 
 runner.notice('Core');
-runner.load('./default/core.js');
+runner.load('./default/sugar-core.js');
 
 runner.notice('Polyfills');
 // Cannot practially run ES5 tests here as resetting
 // native methods breaks the node require system.
-runner.load('./default/es6.js');
-runner.load('./default/es7.js');
+runner.load('./default/sugar-es6.js');
+runner.load('./default/sugar-es7.js');
 
 runner.notice('Default');
-runner.load('./default/array.js');
-runner.load('./default/object.js');
-runner.load('./default/enumerable.js');
-runner.load('./default/date.js');
-runner.load('./default/function.js');
-runner.load('./default/inflections.js');
-runner.load('./default/language.js');
-runner.load('./default/number.js');
-runner.load('./default/range.js');
-runner.load('./default/regexp.js');
-runner.load('./default/string.js');
+runner.load('./default/default.js');
 runner.load('./default/sugar.js');
+runner.load('./default/sugar-array.js');
+runner.load('./default/sugar-object.js');
+runner.load('./default/sugar-enumerable.js');
+runner.load('./default/sugar-date.js');
+runner.load('./default/sugar-function.js');
+runner.load('./default/sugar-inflections.js');
+runner.load('./default/sugar-language.js');
+runner.load('./default/sugar-number.js');
+runner.load('./default/sugar-range.js');
+runner.load('./default/sugar-regexp.js');
+runner.load('./default/sugar-string.js');
 
 runner.notice('Extended');
-runner.load('./extended/array.js');
-runner.load('./extended/object.js');
-runner.load('./extended/enumerable.js');
-runner.load('./extended/date.js');
-runner.load('./extended/function.js');
-runner.load('./extended/inflections.js');
-runner.load('./extended/language.js');
-runner.load('./extended/number.js');
-runner.load('./extended/range.js');
-runner.load('./extended/regexp.js');
-runner.load('./extended/string.js');
+runner.load('./extended/default.js');
 runner.load('./extended/sugar.js');
+runner.load('./extended/sugar-array.js');
+runner.load('./extended/sugar-object.js');
+runner.load('./extended/sugar-enumerable.js');
+runner.load('./extended/sugar-date.js');
+runner.load('./extended/sugar-function.js');
+runner.load('./extended/sugar-inflections.js');
+runner.load('./extended/sugar-language.js');
+runner.load('./extended/sugar-number.js');
+runner.load('./extended/sugar-range.js');
+runner.load('./extended/sugar-regexp.js');
+runner.load('./extended/sugar-string.js');
 
 runner.logTotals();
