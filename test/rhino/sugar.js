c85ae65dbf1fea19a7c857da2516895c628e8a02
Andrew Plummer <plummer.andrew@gmail.com>
2016-01-17 23:43:48 +0900

    adding rhino locale tests and checking

--- test/rhino/sugar.js
+++ test/rhino/sugar.js
@@ -1,10 +1,28 @@
 
 load('sugar.js');
 
+load('locales/ca.js');
+load('locales/da.js');
+load('locales/de.js');
+load('locales/es.js');
+load('locales/fi.js');
+load('locales/fr.js');
+load('locales/it.js');
+load('locales/ja.js');
+load('locales/ko.js');
+load('locales/nl.js');
+load('locales/pl.js');
+load('locales/pt.js');
+load('locales/ru.js');
+load('locales/sv.js');
+load('locales/zh-CN.js');
+load('locales/zh-TW.js');
+
 load('test/suite/suite.js');
 load('test/suite/log.js');
-load('test/suite/helpers/common.js');
 load('test/suite/helpers/core.js');
+load('test/suite/helpers/common.js');
+load('test/suite/helpers/array.js');
 load('test/suite/helpers/date.js');
 load('test/suite/helpers/object.js');
 
@@ -26,18 +44,36 @@ load('test/suite/helpers/object.js');
  *
  */
 
-load('test/tests/array.js');
 load('test/tests/core.js');
-load('test/tests/date-range.js');
+load('test/tests/array.js');
 load('test/tests/date.js');
-load('test/tests/enumerable.js');
 load('test/tests/equals.js');
 // load('test/tests/function.js');
-load('test/tests/number-range.js');
 load('test/tests/number.js');
 load('test/tests/object.js');
 load('test/tests/regexp.js');
-load('test/tests/string-range.js');
 load('test/tests/string.js');
+load('test/tests/enumerable.js');
+
+load('test/tests/number-range.js');
+load('test/tests/string-range.js');
+load('test/tests/date-range.js');
+
+load('test/tests/locales/ca.js');
+load('test/tests/locales/da.js');
+load('test/tests/locales/de.js');
+load('test/tests/locales/es.js');
+load('test/tests/locales/fi.js');
+load('test/tests/locales/fr.js');
+load('test/tests/locales/it.js');
+load('test/tests/locales/ja.js');
+load('test/tests/locales/ko.js');
+load('test/tests/locales/nl.js');
+load('test/tests/locales/pl.js');
+load('test/tests/locales/pt.js');
+load('test/tests/locales/ru.js');
+load('test/tests/locales/sv.js');
+load('test/tests/locales/zh-CN.js');
+load('test/tests/locales/zh-TW.js');
 
 runTests(logResults, false, 'rhino');
